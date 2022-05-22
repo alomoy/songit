cb1e3e0a55adbb127987e16a0ee682c2b8902111
mahmud <almahmud.sbi@gmail.com>
2022-05-23 00:47:00 +0600

    add tariq munawar 1

---
+++ players/tariq-munawar-1.js
@@ -0,0 +1,74 @@
+// Define the tracks that have to be played
+let track_list = [
+  {
+    name: "আল্লাহকে যে পাইতে চায়",
+    artist: "তারিক মুনাওয়ার",
+    album: "গুচ্ছ-১ ",
+    image: "https://images.pexels.com/photos/2264753/pexels-photo-2264753.jpeg?auto=compress&cs=tinysrgb&dpr=3&h=250&w=250",
+    path: "https://www.dropbox.com/s/xpikyzx7pzm449e/01.%20Allahke%20ze%20paite.mp3?dl=1"
+	
+  },
+  {
+    name: "ওরে কে বলে আরবে",
+    artist: "তারিক মুনাওয়ার",
+    album: "গুচ্ছ-১ ",
+    image: "https://images.pexels.com/photos/3100835/pexels-photo-3100835.jpeg?auto=compress&cs=tinysrgb&dpr=3&h=250&w=250",
+    path: "https://www.dropbox.com/s/7dzc43tv2lcc8kc/02.%20ore%20ke%20bole.mp3?dl=1"
+  },
+  {
+    name: "তৌহিদেরই মুর্শিদ আমার",
+    artist: "তারিক মুনাওয়ার",
+    album: "গুচ্ছ-১ ",
+    image: "https://images.pexels.com/photos/1717969/pexels-photo-1717969.jpeg?auto=compress&cs=tinysrgb&dpr=3&h=250&w=250",
+    path: "https://www.dropbox.com/s/vla0k8tvaup56jz/03.%20tauhider%20murshid.mp3?dl=1",
+  },
+  {
+    name: "আল্লাহ রাসুল নামের জপে",
+    artist: "তারিক মুনাওয়ার",
+    album: "গুচ্ছ-১ ",
+    image: "https://images.pexels.com/photos/3100835/pexels-photo-3100835.jpeg?auto=compress&cs=tinysrgb&dpr=3&h=250&w=250",
+    path: "https://www.dropbox.com/s/1ovqvxoi0bnzbgp/04.%20Allah%20rasul%20namer.mp3?dl=1"
+  },
+  {
+    name: "শোনো শোনো ইয়া এলাহী",
+    artist: "তারিক মুনাওয়ার",
+    album: "গুচ্ছ-১ ",
+    image: "https://images.pexels.com/photos/1717969/pexels-photo-1717969.jpeg?auto=compress&cs=tinysrgb&dpr=3&h=250&w=250",
+    path: "https://www.dropbox.com/s/i6m3d7fzfzsdof9/05.%20shono%20shono%20ya%20ilahi.mp3?dl=1",
+  },
+  {
+    name: "সকাল হলো",
+    artist: "তারিক মুনাওয়ার",
+    album: "গুচ্ছ-১ ",
+    image: "https://images.pexels.com/photos/3100835/pexels-photo-3100835.jpeg?auto=compress&cs=tinysrgb&dpr=3&h=250&w=250",
+    path: "https://www.dropbox.com/s/xotk4e0u06w5ffj/06.%20sokal%20holo%20shunre%20azan.mp3?dl=1"
+  },
+  {
+    name: "এই সুন্দর ফল",
+    artist: "তারিক মুনাওয়ার",
+    album: "গুচ্ছ-১ ",
+    image: "https://images.pexels.com/photos/1717969/pexels-photo-1717969.jpeg?auto=compress&cs=tinysrgb&dpr=3&h=250&w=250",
+    path: "https://www.dropbox.com/s/gqdzoo0g0het4zx/08.%20ei%20sundor%20fol.mp3?dl=1",
+  },
+  {
+    name: "পূবাল হাওয়া",
+    artist: "তারিক মুনাওয়ার",
+    album: "গুচ্ছ-১ ",
+    image: "https://images.pexels.com/photos/2264753/pexels-photo-2264753.jpeg?auto=compress&cs=tinysrgb&dpr=3&h=250&w=250",
+    path: "https://www.dropbox.com/s/lm5cjdcca1i28an/07.%20pubal%20hawa.mp3?dl=1"
+  },
+  {
+    name: "খোদার প্রেমের",
+    artist: "তারিক মুনাওয়ার",
+    album: "গুচ্ছ-১ ",
+    image: "https://images.pexels.com/photos/3100835/pexels-photo-3100835.jpeg?auto=compress&cs=tinysrgb&dpr=3&h=250&w=250",
+    path: "https://www.dropbox.com/s/s7lrvvre81wxy46/09.%20khodar%20premer.mp3?dl=1"
+  },
+  {
+    name: "চলরে কাবার জিয়ারতে",
+    artist: "তারিক মুনাওয়ার",
+    album: "গুচ্ছ-১ ",
+    image: "https://images.pexels.com/photos/1717969/pexels-photo-1717969.jpeg?auto=compress&cs=tinysrgb&dpr=3&h=250&w=250",
+    path: "https://www.dropbox.com/s/x67432jkfzonwor/10.%20cholre%20kabaar%20jiarote.mp3?dl=1",
+  },
+];
