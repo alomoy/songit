957126f51ea8a329efe8edde2e7e8a7570c8a212
mahmud <almahmud.sbi@gmail.com>
2022-05-26 10:41:49 +0600

    add tariq munawar 3

---
+++ players/tariq-munawar-3.js
@@ -0,0 +1,109 @@
+// Define the tracks that have to be played
+let track_list = [
+  {
+    name: "সারা জীবন আমি",
+    artist: "তারিক মুনাওয়ার",
+    album: "গুচ্ছ - ৩ ",
+    image: "../images/karakoram.jpg",
+    path: "https://www.dropbox.com/s/peaa1q5to6rh3gs/26-sara-jibon-ami.mp3?dl=1"
+	
+  },
+  {
+    name: "গান দাও আমার",
+    artist: "তারিক মুনাওয়ার",
+    album: "গুচ্ছ - ৩ ",
+    image: "../images/karakoram.jpg",
+    path: "https://www.dropbox.com/s/w2zjort6rz1f8sq/27-gan-dao-amay.mp3?dl=1" 
+  },
+  {
+    name: "হে খোদা মাফ",
+    artist: "তারিক মুনাওয়ার",
+    album: "গুচ্ছ - ৩ ",
+    image: "../images/mount.jpg",
+    path: "https://www.dropbox.com/s/p2tev504m6z8ij9/28-he-khoda-maf.mp3?dl=1",
+  },
+  {
+    name: "আল্লাহ আমার প্রভু",
+    artist: "তারিক মুনাওয়ার",
+    album: "গুচ্ছ - ৩ ",
+    image: "../images/karakoram.jpg",
+    path: "https://www.dropbox.com/s/q0ie88hnc28t7g1/29-Allah-amar-provu.mp3?dl=1"
+  },
+  {
+    name: "বাজলো কিরে ভোরের",
+    artist: "তারিক মুনাওয়ার",
+    album: "গুচ্ছ - ৩ ",
+    image: "../images/mount.jpg",
+    path: "https://www.dropbox.com/s/5725kyvm126n19d/30-bajlo-kire-vorer.mp3?dl=1",
+  },
+  {
+    name: "মুহাম্মাদের নাম",
+    artist: "তারিক মুনাওয়ার",
+    album: "গুচ্ছ - ৩ ",
+    image: "../images/karakoram.jpg",
+    path: "https://www.dropbox.com/s/msw87eqwgtjajw0/31-Muhammader-nam.mp3?dl=1"
+  },
+  {
+    name: "ইসলামের সওদা",
+    artist: "তারিক মুনাওয়ার",
+    album: "গুচ্ছ - ৩ ",
+    image: "../images/mount.jpg",
+    path: "https://www.dropbox.com/s/k7xhzep7aggjkbh/32-islamer-souda.mp3?dl=1",
+  },
+  {
+    name: "ধর্মের পথে শহিদ",
+    artist: "তারিক মুনাওয়ার",
+    album: "গুচ্ছ - ৩ ",
+    image: "../images/light.jpg",
+    path: "https://www.dropbox.com/s/zmh6za282862giv/33-dhormer-pothe.mp3?dl=1"
+  },
+  {
+    name: "সাহারাতে ফুটলো ফুল",
+    artist: "তারিক মুনাওয়ার",
+    album: "গুচ্ছ - ৩ ",
+    image: "../images/karakoram.jpg",
+    path: "https://www.dropbox.com/s/up4zvlvs673ktsf/34-saharate-futlo.mp3?dl=1"
+  },
+  {
+    name: "এই গরীবের শোনো",
+    artist: "তারিক মুনাওয়ার",
+    album: "গুচ্ছ - ৩ ",
+    image: "../images/mount.jpg",
+    path: "https://www.dropbox.com/s/bp4ia7vsnzm45lx/35-ei-goriber-shono.mp3?dl=1",
+  },
+   {
+    name: "আল্লাহতে যার পূর্ণ",
+    artist: "তারিক মুনাওয়ার",
+    album: "গুচ্ছ - ৩ ",
+    image: "../images/karakoram.jpg",
+    path: "https://www.dropbox.com/s/8qmgh6t93141t7x/36-Allah-te-zar-purno.mp3?dl=1"
+  },
+  {
+    name: "ত্রিভূবনের প্রিয় মুহাম্মাদ",
+    artist: "তারিক মুনাওয়ার",
+    album: "গুচ্ছ - ৩ ",
+	image: "../images/mount.jpg",
+    path: "https://www.dropbox.com/s/otcn4ezoziw1yoy/37-trivuboner-priyo.mp3?dl=1",
+  },
+   {
+    name: "রোজ হাশরে আল্লাহ",
+    artist: "তারিক মুনাওয়ার",
+    album: "গুচ্ছ - ৩ ",
+    image: "../images/karakoram.jpg",
+    path: "https://www.dropbox.com/s/uecpj4ikv3gmt8a/38-roj-hashore-Allah.mp3?dl=1"
+  },
+  {
+    name: "বাজিছে দামামা",
+    artist: "তারিক মুনাওয়ার",
+    album: "গুচ্ছ - ৩ ",
+    image: "../images/mount.jpg",
+    path: "https://www.dropbox.com/s/qdyw32u6khfgwjb/39-bajiche-damama.mp3?dl=1",
+  },
+    {
+    name: "বক্ষে আমার কাবার",
+    artist: "তারিক মুনাওয়ার",
+    album: "গুচ্ছ - ৩ ",
+    image: "../images/light.jpg",
+    path: "https://www.dropbox.com/s/gciprv4musl0mhx/40-bokkhe-amar-kabar.mp3?dl=1",
+  },
+];
