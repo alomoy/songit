60b9501cef54e7dc5a8fbb22eb8ad9695ec7368e
mahmud <almahmud.sbi@gmail.com>
2022-05-26 22:05:52 +0600

    add uddipon and polligiti

---
+++ players/ke-elo-madinay.js
@@ -0,0 +1,10 @@
+// Define the tracks that have to be played
+let track_list = [
+  {
+    name: "বিবিধ",
+    artist: "মশিউর রহমান",
+    album: "অ্যালবাম: কে এলো মদীনায়",
+    image: "../images/album/ke-elo-madinay.jpg",
+    path: "https://www.dropbox.com/s/4kyu3wk51a6okoz/polligit_ke_elo_modinay.mp3?dl=1"
+  },
+];
---
+++ players/uddipon.js
@@ -0,0 +1,10 @@
+// Define the tracks that have to be played
+let track_list = [
+  {
+    name: "বিবিধ",
+    artist: "মশিউর রহমান",
+    album: "অ্যালবাম: উদ্দীপন",
+    image: "../images/album/uddipon.jpg",
+    path: "https://www.dropbox.com/s/3mhowta4410kg7g/Uddipon_moshiur.mp3?dl=1"
+  },
+];
