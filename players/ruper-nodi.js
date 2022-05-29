874d27f38fe1b14eae93518ef57b515ecdb5bdb9
mahmud <almahmud.sbi@gmail.com>
2022-05-29 21:14:24 +0600

    Add ruper nodi

---
+++ players/ruper-nodi.js
@@ -0,0 +1,102 @@
+// Define the tracks that have to be played
+let track_list = [
+  {
+    name: "তাকবীর তোলো",
+    artist: "সাইমুম শিল্পী গোষ্ঠী",
+    album: "অ্যালবাম: রূপের নদী (সাইমুম-৪১) ",
+    image: "../images/karakoram.jpg",
+    path: "https://www.dropbox.com/s/75matwzu9szxka6/1-takbir-tolo.mp3?dl=1"
+  },
+  {
+    name: "আমার যে কথা কাজে",
+    artist: "সাইমুম শিল্পী গোষ্ঠী",
+    album: "অ্যালবাম: রূপের নদী (সাইমুম-৪১) ",
+    image: "../images/mount.jpg",
+    path: "https://www.dropbox.com/s/1vcfyiwu6kdtjhv/2-amar-ze-kotha.mp3?dl=1"
+  },
+  {
+    name: "আমরা শিশু",
+    artist: "সাইমুম শিল্পী গোষ্ঠী",
+    album: "অ্যালবাম: রূপের নদী (সাইমুম-৪১) ",
+    image: "../images/karakoram.jpg",
+    path: "https://www.dropbox.com/s/2vqiqy739cj68e2/3-amra-shishu.mp3?dl=1",
+  },
+    {
+    name: "বাগানের ফুলগুলো",
+    artist: "সাইমুম শিল্পী গোষ্ঠী",
+    album: "অ্যালবাম: রূপের নদী (সাইমুম-৪১) ",
+    image: "../images/karakoram.jpg",
+    path: "https://www.dropbox.com/s/8lgv4gm70rcx3g3/4-baganer-fulgulo.mp3?dl=1",
+  },
+    {
+    name: "এই যে মোগো গাঁও",
+    artist: "সাইমুম শিল্পী গোষ্ঠী",
+    album: "অ্যালবাম: রূপের নদী (সাইমুম-৪১) ",
+    image: "../images/karakoram.jpg",
+    path: "https://www.dropbox.com/s/9e44vgksbuhkgds/5-ei-ze-mogo-gaon.mp3?dl=1",
+  },
+    {
+    name: "হৃদয় গহীনে",
+    artist: "সাইমুম শিল্পী গোষ্ঠী",
+    album: "অ্যালবাম: রূপের নদী (সাইমুম-৪১) ",
+    image: "../images/karakoram.jpg",
+    path: "https://www.dropbox.com/s/w9gj79zfvgos9m4/6-hridoy-gohine.mp3?dl=1"
+	
+  },
+  {
+    name: "কল কল করে রূপের নদী",
+    artist: "সাইমুম শিল্পী গোষ্ঠী",
+    album: "অ্যালবাম: রূপের নদী (সাইমুম-৪১) ",
+    image: "../images/mount.jpg",
+    path: "https://www.dropbox.com/s/k8f4dke1lkz9z3s/7-kolo-kolo-kore.mp3?dl=1"
+  },
+  {
+    name: "লা তানসা",
+    artist: "সাইমুম শিল্পী গোষ্ঠী",
+    album: "অ্যালবাম: রূপের নদী (সাইমুম-৪১) ",
+    image: "../images/karakoram.jpg",
+    path: "https://www.dropbox.com/s/voyvzscvlcc8e8q/8-la-tan-sa.mp3?dl=1",
+  },
+  {
+    name: "লক্ষ গানের শিল্পী",
+    artist: "সাইমুম শিল্পী গোষ্ঠী",
+    album: "অ্যালবাম: রূপের নদী (সাইমুম-৪১) ",
+    image: "../images/karakoram.jpg",
+    path: "https://www.dropbox.com/s/iakuxgm1229gan5/9-lokkho-ganer-shilpi.mp3?dl=1",
+  },
+  {
+    name: "মা হলো শান্তি",
+    artist: "সাইমুম শিল্পী গোষ্ঠী",
+    album: "অ্যালবাম: রূপের নদী (সাইমুম-৪১) ",
+    image: "../images/karakoram.jpg",
+    path: "https://www.dropbox.com/s/rt98519doiqoqho/10-ma-holo-shanti.mp3?dl=1",
+  },
+    {
+    name: "মৌমাছি যাও",
+    artist: "সাইমুম শিল্পী গোষ্ঠী",
+    album: "অ্যালবাম: রূপের নদী (সাইমুম-৪১) ",
+    image: "../images/karakoram.jpg",
+    path: "https://www.dropbox.com/s/g3heomgqqctwz5d/11-moumachi.mp3?dl=1",
+  },
+    {
+    name: "মাই মাদার ইজ",
+    artist: "সাইমুম শিল্পী গোষ্ঠী",
+    album: "অ্যালবাম: রূপের নদী (সাইমুম-৪১) ",
+    image: "../images/karakoram.jpg",
+    path: "https://www.dropbox.com/s/722khs1frymy2o6/12-my-mother.mp3?dl=1",
+  },
+    {
+    name: "নদীর কলতানে",
+    artist: "সাইমুম শিল্পী গোষ্ঠী",
+    album: "অ্যালবাম: রূপের নদী (সাইমুম-৪১) ",
+    image: "../images/karakoram.jpg",
+    path: "https://www.dropbox.com/s/v8jmwtcrzgtxsj8/13-nodir-kolotane.mp3?dl=1",
+  },
+    {
+    name: "শিল্পী বলো কারে",
+    artist: "সাইমুম শিল্পী গোষ্ঠী",
+    album: "অ্যালবাম: রূপের নদী (সাইমুম-৪১) ",
+    image: "../images/karakoram.jpg",
+    path: "https://www.dropbox.com/s/yumf7wnpo9k2txc/14-shilpi-bolo-kare.mp3?dl=1",
+  },
+];
