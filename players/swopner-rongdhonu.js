b2776a0feccd9fcfb70753741e2676a50982364b
mahmud <almahmud.sbi@gmail.com>
2022-05-29 21:45:43 +0600

    Add swopner rongdhonu

---
+++ players/swopner-rongdhonu.js
@@ -0,0 +1,102 @@
+// Define the tracks that have to be played
+let track_list = [
+  {
+    name: "এই ধরণী ও নীল",
+    artist: "সাইমুম শিল্পী গোষ্ঠী",
+    album: "অ্যালবাম: স্বপ্নের রংধনু",
+    image: "../images/karakoram.jpg",
+    path: "https://www.dropbox.com/s/axx705ho77qlq8r/1ei%20dhorini%20oi%20nil.mp3?dl=1"
+  },
+  {
+    name: "ধরাতে আগমনে পিয়ারা",
+    artist: "সাইমুম শিল্পী গোষ্ঠী",
+    album: "অ্যালবাম: স্বপ্নের রংধনু",
+    image: "../images/train.jpg",
+    path: "https://www.dropbox.com/s/uzmc8gqo46twut2/2dhorate%20agomone.mp3?dl=1"
+  },
+  {
+    name: "এই দেশ আমার",
+    artist: "সাইমুম শিল্পী গোষ্ঠী",
+    album: "অ্যালবাম: স্বপ্নের রংধনু",
+    image: "../images/mount.jpg",
+    path: "https://www.dropbox.com/s/apmhh19tyrxat0t/3%20ei%20desh%20amar.mp3?dl=1",
+  },
+    {
+    name: "আমার মা আমার",
+    artist: "সাইমুম শিল্পী গোষ্ঠী",
+    album: "অ্যালবাম: স্বপ্নের রংধনু",
+    image: "../images/karakoram.jpg",
+    path: "https://www.dropbox.com/s/rj2amdiki4fstpn/4o%20amar%20ma.mp3?dl=1",
+  },
+    {
+    name: "আমরা সবাই এক",
+    artist: "সাইমুম শিল্পী গোষ্ঠী",
+    album: "অ্যালবাম: স্বপ্নের রংধনু",
+    image: "../images/tunnel.jpg",
+    path: "https://www.dropbox.com/s/97gjq888nl1w5wh/5amra%20sobai%20-ek.mp3?dl=1",
+  },
+    {
+    name: "কিছু কিছু কথা",
+    artist: "সাইমুম শিল্পী গোষ্ঠী",
+    album: "অ্যালবাম: স্বপ্নের রংধনু",
+    image: "../images/mount.jpg",
+    path: "https://www.dropbox.com/s/kwzp5ecnvae6eje/6kichu%20kichu.mp3?dl=1"
+	
+  },
+  {
+    name: "ওরে বাংলার মুসলিম",
+    artist: "সাইমুম শিল্পী গোষ্ঠী",
+    album: "অ্যালবাম: স্বপ্নের রংধনু",
+    image: "../images/karakoram.jpg",
+    path: "https://www.dropbox.com/s/c792uvbfd8gibxs/7ore%20banglar%20muslim.mp3?dl=1"
+  },
+  {
+    name: "সাগর নদী আর পাহাড়",
+    artist: "সাইমুম শিল্পী গোষ্ঠী",
+    album: "অ্যালবাম: স্বপ্নের রংধনু",
+    image: "../images/karakoram.jpg",
+    path: "https://www.dropbox.com/s/aqfe4riat6q7zl0/8sagor%20nodi%20chute.mp3?dl=1",
+  },
+  {
+    name: "জানি না আর ফুটবে কি না",
+    artist: "সাইমুম শিল্পী গোষ্ঠী",
+    album: "অ্যালবাম: স্বপ্নের রংধনু",
+    image: "../images/light.jpg",
+    path: "https://www.dropbox.com/s/ve0v07mkaf4ez3l/9janina%20ar.mp3?dl=1",
+  },
+  {
+    name: "তুমি মালিক",
+    artist: "সাইমুম শিল্পী গোষ্ঠী",
+    album: "অ্যালবাম: স্বপ্নের রংধনু",
+    image: "../images/karakoram.jpg",
+    path: "https://www.dropbox.com/s/xoftil65s3l7irq/10tumi%20malik%20tumi.mp3?dl=1",
+  },
+    {
+    name: "সারাটা পৃথিবী কারবালা",
+    artist: "সাইমুম শিল্পী গোষ্ঠী",
+    album: "অ্যালবাম: স্বপ্নের রংধনু",
+    image: "../images/karakoram.jpg",
+    path: "https://www.dropbox.com/s/bahk2agixmz8gkv/11sarata%20prithibi%20aj.mp3?dl=1",
+  },
+    {
+    name: "নদীরে তুই",
+    artist: "সাইমুম শিল্পী গোষ্ঠী",
+    album: "অ্যালবাম: স্বপ্নের রংধনু",
+    image: "../images/karakoram.jpg",
+    path: "https://www.dropbox.com/s/625f86wkybi8nhw/12nodire%20tui.mp3?dl=1",
+  },
+    {
+    name: "ইয়া সায়্যিদি",
+    artist: "সাইমুম শিল্পী গোষ্ঠী",
+    album: "অ্যালবাম: স্বপ্নের রংধনু",
+    image: "../images/karakoram.jpg",
+    path: "https://www.dropbox.com/s/i0dr05tl4xrz2ja/13ya%20sayeedi.mp3?dl=1",
+  },
+    {
+    name: "এই ওলী আল্লাহর",
+    artist: "সাইমুম শিল্পী গোষ্ঠী",
+    album: "অ্যালবাম: স্বপ্নের রংধনু",
+    image: "../images/karakoram.jpg",
+    path: "https://www.dropbox.com/s/m1i6esb9747ktq6/14ei%20wali-allahr.mp3?dl=0",
+  },
+];
