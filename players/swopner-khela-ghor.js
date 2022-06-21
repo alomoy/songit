81f6db32d49da902cee3f0359e050955f6c69a37
mahmud <almahmud.sbi@gmail.com>
2022-06-21 20:33:32 +0600

    add swopner khela ghor-didarul

---
+++ players/swopner-khela-ghor.js
@@ -0,0 +1,88 @@
+// Define the tracks that have to be played
+let track_list = [
+  {
+    name: "আল্লাহু আকবর",
+    artist: "দিদারুল ইসলাম-সাইমুম শিল্পী গোষ্ঠী",
+    album: "অ্যালবাম: স্বপ্নের খেলা ঘর",
+    image: "../images/karakoram.jpg",
+    path: "https://www.dropbox.com/s/j7qvdejzc3zgvwm/1-Allahu-akbar-.mp3?dl=0"
+  },
+  {
+    name: "কে যাও কাবার",
+    artist: "দিদারুল ইসলাম-সাইমুম শিল্পী গোষ্ঠী",
+    album: "অ্যালবাম: স্বপ্নের খেলা ঘর",
+    image: "../images/train.jpg",
+    path: "https://www.dropbox.com/s/d8f6qmyzh99926h/2-ke-zao-kabar-pothe-.mp3?dl=0"
+  },
+  {
+    name: "ইসলাম নয় ধর্মের নাম",
+    artist: "দিদারুল ইসলাম-সাইমুম শিল্পী গোষ্ঠী",
+    album: "অ্যালবাম: স্বপ্নের খেলা ঘর",
+    image: "../images/mount.jpg",
+    path: "https://www.dropbox.com/s/l3mkuoqqed10o6e/3-islamo-noy-dhormer-.mp3?dl=0",
+  },
+    {
+    name: "আগুনে পোড়া মন",
+    artist: "দিদারুল ইসলাম-সাইমুম শিল্পী গোষ্ঠী",
+    album: "অ্যালবাম: স্বপ্নের খেলা ঘর",
+    image: "../images/karakoram.jpg",
+    path: "https://www.dropbox.com/s/3a51ih3wqa6n4qm/4-agune-pora.mp3?dl=0",
+  },
+    {
+    name: "মাঝে মাঝে মনটা",
+    artist: "দিদারুল ইসলাম-সাইমুম শিল্পী গোষ্ঠী",
+    album: "অ্যালবাম: স্বপ্নের খেলা ঘর",
+    image: "../images/tunnel.jpg",
+    path: "https://www.dropbox.com/s/8fo61ekmiyyx4la/5-majhe-majhe-monta.mp3?dl=0",
+  },
+    {
+    name: "যদি হয় স্বাধীনতা",
+    artist: "দিদারুল ইসলাম-সাইমুম শিল্পী গোষ্ঠী",
+    album: "অ্যালবাম: স্বপ্নের খেলা ঘর",
+    image: "../images/mount.jpg",
+    path: "https://www.dropbox.com/s/i26ar6kx3i5cd6t/7-swadhinota.mp3?dl=0"
+	
+  },
+  {
+    name: "জীবন নামের",
+    artist: "দিদারুল ইসলাম-সাইমুম শিল্পী গোষ্ঠী",
+    album: "অ্যালবাম: স্বপ্নের খেলা ঘর",
+    image: "../images/karakoram.jpg",
+    path: "https://www.dropbox.com/s/qp7l4733d09xnpr/8-jibon-namer.mp3?dl=0"
+  },
+  {
+    name: "আমার দিন কি",
+    artist: "দিদারুল ইসলাম-সাইমুম শিল্পী গোষ্ঠী",
+    album: "অ্যালবাম: স্বপ্নের খেলা ঘর",
+    image: "../images/light.jpg",
+    path: "https://www.dropbox.com/s/i9t1o3uvy5h0vkm/9-amar-din-ki.mp3?dl=0",
+  },
+  {
+    name: "আসমানী কুরআন",
+    artist: "দিদারুল ইসলাম-সাইমুম শিল্পী গোষ্ঠী",
+    album: "অ্যালবাম: স্বপ্নের খেলা ঘর",
+    image: "../images/karakoram.jpg",
+    path: "https://www.dropbox.com/s/t29kqpvhl2pqa24/10-asmani-quran.mp3?dl=0",
+  },
+    {
+    name: "ও মাঝি ভাই",
+    artist: "দিদারুল ইসলাম-সাইমুম শিল্পী গোষ্ঠী",
+    album: "অ্যালবাম: স্বপ্নের খেলা ঘর",
+    image: "../images/tunnel.jpg",
+    path: "https://www.dropbox.com/s/g7asdjnlg5q1kfr/11-o-majhi-bhai-.mp3?dl=0",
+  },
+    {
+    name: "সে কোন বন্ধু",
+    artist: "দিদারুল ইসলাম-সাইমুম শিল্পী গোষ্ঠী",
+    album: "অ্যালবাম: স্বপ্নের খেলা ঘর",
+    image: "../images/karakoram.jpg",
+    path: "https://www.dropbox.com/s/5wziq2mchvutear/12-se-kon-bondhu-.mp3?dl=0",
+  },
+    {
+    name: "একদিন আমি হারিয়ে",
+    artist: "দিদারুল ইসলাম-সাইমুম শিল্পী গোষ্ঠী",
+    album: "অ্যালবাম: স্বপ্নের খেলা ঘর",
+    image: "../images/tunnel.jpg",
+    path: "https://www.dropbox.com/s/fu2sho9s8racqpc/13-ekdin-ami-hariye.mp3?dl=0",
+  },
+];
