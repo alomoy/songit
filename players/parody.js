d9f637a3fd81b48a1966ad381060011609c5b46b
mahmud <almahmud.sbi@gmail.com>
2022-06-29 00:25:41 +0600

    Add parody gaan

---
+++ players/parody.js
@@ -0,0 +1,46 @@
+// Define the tracks that have to be played
+let track_list = [
+  {
+    name: "আমার দয়াল আল্লাহরে",
+    artist: "শিল্পী আবুল কাশেম - সাইমুম শিল্পী গোষ্ঠী",
+    album: "অ্যালবাম: প্যারোডি গান",
+    image: "../images/karakoram.jpg",
+    path: "https://www.dropbox.com/s/5rjwk8ynbko9wnp/1-amar-doyal-Allahre.mp3?dl=1"
+  },
+  {
+    name: "চলছে মামু ভাগিনার কারবার",
+    artist: "সাইমুম শিল্পী গোষ্ঠী",
+    album: "অ্যালবাম: প্যারোডি গান",
+    image: "../images/train.jpg",
+    path: "https://www.dropbox.com/s/rtj4f5o58o6imw7/2-mamu-vaginar-karbar.mp3?dl=1"
+  },
+  {
+    name: "দুনিয়ার গোলক ধাঁধাঁয়",
+    artist: "সাইমুম শিল্পী গোষ্ঠী",
+    album: "অ্যালবাম: প্যারোডি গান",
+    image: "../images/mount.jpg",
+    path: "https://www.dropbox.com/s/bw4i90186zgto4h/3-duniar-golok-dhandha.mp3?dl=1",
+  },
+    {
+    name: "সাধের সুখ পাখি",
+    artist: "সাইমুম শিল্পী গোষ্ঠী",
+    album: "অ্যালবাম: প্যারোডি গান",
+    image: "../images/karakoram.jpg",
+    path: "https://www.dropbox.com/s/6iezi9u8bfdvnrr/4-sadher-sukh-pakhi.mp3?dl=1",
+  },
+    {
+    name: "প্রাণ দিয়াছেন যিনি আমারে",
+    artist: "সাইমুম শিল্পী গোষ্ঠী",
+    album: "অ্যালবাম: প্যারোডি গান",
+    image: "../images/tunnel.jpg",
+    path: "https://www.dropbox.com/s/6dhttzpg79rrb8j/5-pran-diachen-zini-amare.mp3?dl=1",
+  },
+    {
+    name: "ওরে মিছে মায়ায় ডুবে",
+    artist: "সাইমুম শিল্পী গোষ্ঠী",
+    album: "অ্যালবাম: প্যারোডি গান",
+    image: "../images/mount.jpg",
+    path: "https://www.dropbox.com/s/ahyr1vmb7t8wf2q/6-ore-miche-mayay.mp3?dl=1"
+	
+  },
+];
