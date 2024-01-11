33fb4fce9c40b0507f7a723f585b2a265892db72
Abdullah Al Mahmud <mahmud@pop-os.localdomain>
2024-01-11 17:18:08 +0600

    Add kiser bytha baje

---
+++ players/kiser-bytha-baje.js
@@ -0,0 +1,96 @@
+// Define the tracks that have to be played
+let track_list = [
+  {
+    name: "যেমন তুমি দিলে যমীন",
+    artist: "মশিউর রহমান ",
+    album: "অ্যালবাম: কিসের ব্যথা বাজে",
+    image: "../images/karakoram.jpg",
+    path: "https://dl.dropbox.com/s/a4hy1ycvqam144p/01%20-Jemon%20tumi%20dile%20jomin.mp3?dl=1"
+  },
+  {
+    name: "একটা ওমর এ যমানায়",
+    artist: "মশিউর রহমান ",
+    album: "অ্যালবাম: কিসের ব্যথা বাজে",
+    image: "../images/train.jpg",
+    path: "https://dl.dropbox.com/s/lzud8hh80snx4kx/02%20-%20Akta%20omar%20ai%20jamanai.mp3?dl=1"
+  },
+  {
+    name: "সোনার পাখিরা একে একে",
+    artist: "মশিউর রহমান ",
+    album: "অ্যালবাম: কিসের ব্যথা বাজে",
+    image: "../images/mount.jpg",
+    path: "https://dl.dropbox.com/s/6kkdlhqlfy6hr65/03%20-Sonar%20pakhira.mp3?dl=1",
+  },
+    {
+    name: "আমার কণ্ঠে এমন সুধা দাও",
+    artist: "মশিউর রহমান ",
+    album: "অ্যালবাম: কিসের ব্যথা বাজে",
+    image: "../images/karakoram.jpg",
+    path: "https://dl.dropbox.com/s/9pqn5w0du3fuful/04%20-Amar%20kondhe%20amon.mp3?dl=1",
+  },
+    {
+    name: "কিসের ব্যথা বাজে!",
+    artist: "মশিউর রহমান ",
+    album: "অ্যালবাম: কিসের ব্যথা বাজে",
+    image: "../images/tunnel.jpg",
+    path: "https://dl.dropbox.com/s/sjg0rifc5zx57m5/06-Solat%20sola.mp3?dl=1",
+  },
+    {
+    name: "কুর’আনের বাণী",
+    artist: "মশিউর রহমান ",
+    album: "অ্যালবাম: কিসের ব্যথা বাজে",
+    image: "../images/mount.jpg",
+    path: "https://dl.dropbox.com/s/0ihwlsh1jzpo9vi/06%20-Koraner%20bani%20je.mp3?dl=1"
+	
+  },
+    {
+    name: "তাঁর দু’হাতে নিয়ামত",
+    artist: "মশিউর রহমান ",
+    album: "অ্যালবাম: কিসের ব্যথা বাজে",
+    image: "../images/karakoram.jpg",
+    path: "https://dl.dropbox.com/s/wf81kop3zsa260m/07%20-%20Tar%20du%20hate.mp3?dl=1"
+  },
+  {
+    name: "শাহাদাত ঈমানের",
+    artist: "মশিউর রহমান ",
+    album: "অ্যালবাম: কিসের ব্যথা বাজে",
+    image: "../images/train.jpg",
+    path: "https://dl.dropbox.com/s/ko7rawwjfw4wrge/08%20-%20Sahadat%20imaner.mp3?dl=1"
+  },
+  {
+    name: "মাঝিরে তুই",
+    artist: "মশিউর রহমান ",
+    album: "অ্যালবাম: কিসের ব্যথা বাজে",
+    image: "../images/mount.jpg",
+    path: "https://dl.dropbox.com/s/cel2lgb1njnpqyp/09%20-Majhire%20tui%20mon.mp3?dl=1",
+  },
+    {
+    name: "আমার মায়ের কথা",
+    artist: "মশিউর রহমান ",
+    album: "অ্যালবাম: কিসের ব্যথা বাজে",
+    image: "../images/karakoram.jpg",
+    path: "https://dl.dropbox.com/s/j5xflxoqzwk5f76/10%20-%20Amar%20mayer%20kotha.mp3?dl=1",
+  },
+    {
+    name: "চলো তবে জেগে উঠি",
+    artist: "মশিউর রহমান ",
+    album: "অ্যালবাম: কিসের ব্যথা বাজে",
+    image: "../images/tunnel.jpg",
+    path: "https://dl.dropbox.com/s/oc4q3m8jos8y302/11%20-Cholo%20tobe%20jage.mp3?dl=1",
+  },
+    {
+    name: "আসবে আবার ওমর",
+    artist: "মশিউর রহমান ",
+    album: "অ্যালবাম: কিসের ব্যথা বাজে",
+    image: "../images/mount.jpg",
+    path: "https://dl.dropbox.com/s/l0qa2nujo5pzon1/12%20-%20Asbe%20abar%20omar.mp3?dl=1"
+	
+  },
+    {
+    name: "হাজার গানের মাঝে",
+    artist: "মশিউর রহমান ",
+    album: "অ্যালবাম: কিসের ব্যথা বাজে",
+    image: "../images/mount.jpg",
+    path: "https://dl.dropbox.com/s/pwrfvzvd04bonxz/13%20-%20Hazar%20ganer%20majhe.mp3?dl=1",
+  },
+];
