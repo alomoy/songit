0103a8260ea1961c996cfa748e93f800764e9713
Abdullah Al Mahmud <mahmud@pop-os.localdomain>
2024-01-12 11:01:52 +0600

    Add rodela dupur

---
+++ players/rodela-dupur.js
@@ -0,0 +1,94 @@
+// Define the tracks that have to be played
+let track_list = [
+  {
+    name: "সুন্দর আহবানে",
+    artist: "সন্দীপন শিল্পী গোষ্ঠী",
+    album: "রোদেলা দুপুর",
+    image: "../images/curvedroad.jpg",
+    path: "https://dl.dropbox.com/s/zxq98fomyt3lhve/01%20Shundor%20aovane.mp3?dl=1"
+  },
+   {
+    name: "তোমার নূরের রোশনীতে",
+    artist: "সন্দীপন শিল্পী গোষ্ঠী",
+    album: "রোদেলা দুপুর",
+    image: "../images/mount.jpg",
+    path: "https://dl.dropbox.com/s/yc9z3qz985ny5km/02%20Tomar%20nurer.mp3?dl=1"
+  },
+  {
+    name: "পৃথিবীর হাজারো কাজের ভীড়ে",
+    artist: "সন্দীপন শিল্পী গোষ্ঠী",
+    album: "রোদেলা দুপুর",
+    image: "../images/curvedroad.jpg",
+    path: "https://dl.dropbox.com/s/ea0qv044ker0731/03Prithibir%20hajaro.mp3?dl=1"
+  },
+  {
+    name: "ও দেশের জনগণ",
+    artist: "সন্দীপন শিল্পী গোষ্ঠী",
+    album: "রোদেলা দুপুর",
+    image: "../images/hillroad.jpg",
+    path: "https://dl.dropbox.com/s/kmok1wstrfikmie/04%20O%20desher.mp3?dl=1"
+  },
+  {
+    name: "শহীদের পথ ধরে আসবে বিজয়",
+    artist: "সন্দীপন শিল্পী গোষ্ঠী",
+    album: "রোদেলা দুপুর",
+    image: "../images/mount.jpg",
+    path: "https://dl.dropbox.com/s/ynw0r1awmqj1uat/05%20Shohider%20poth.mp3?dl=1"
+  },
+  {
+    name: "এক ঝাঁক পায়রা",
+    artist: "সন্দীপন শিল্পী গোষ্ঠী",
+    album: "রোদেলা দুপুর",
+    image: "../images/hillroad.jpg",
+    path: "https://dl.dropbox.com/s/vjau0n0afdrruyq/06Ek%20jhak%20paira.mp3?dl=1"
+  },
+   {
+    name: "রোদেলা দুপুর",
+    artist: "সন্দীপন শিল্পী গোষ্ঠী",
+    album: "রোদেলা দুপুর",
+    image: "../images/curvedroad.jpg",
+    path: "https://dl.dropbox.com/s/9fhlkmu2kjtq9k8/07Rodela%20dupur.mp3?dl=1"
+  },
+  {
+    name: "মহান প্রভূর নামে গানের শুরু",
+    artist: "সন্দীপন শিল্পী গোষ্ঠী",
+    album: "রোদেলা দুপুর",
+    image: "../images/hillroad.jpg",
+    path: "https://dl.dropbox.com/s/3qj6uijr932ahpq/08%20Mohan%20provur.mp3?dl=1"
+  },
+   {
+    name: "পাষাণ পাষাণ মন",
+    artist: "সন্দীপন শিল্পী গোষ্ঠী",
+    album: "রোদেলা দুপুর",
+    image: "../images/mtvill.jpg",
+    path: "https://dl.dropbox.com/s/3even6nwx7mmrts/09%20Pashan%20pashan.mp3?dl=1"
+  },
+  {
+    name: "আমার গানের ভাষা",
+    artist: "সন্দীপন শিল্পী গোষ্ঠী",
+    album: "রোদেলা দুপুর",
+    image: "../images/hillroad.jpg",
+    path: "https://dl.dropbox.com/s/jchdzh32ezelgui/10%20Amar%20gaaner.mp3?dl=1"
+  },
+  {
+    name: "Revolution",
+    artist: "সন্দীপন শিল্পী গোষ্ঠী",
+    album: "রোদেলা দুপুর",
+    image: "../images/karakoram.jpg",
+    path: "https://dl.dropbox.com/s/c9wvnjio2aeqpxh/11%20Revolution.mp3?dl=1"
+  },
+   {
+    name: "তুমি কাঁঠাল পাতার রং",
+    artist: "সন্দীপন শিল্পী গোষ্ঠী",
+    album: "রোদেলা দুপুর",
+    image: "../images/hillroad.jpg",
+    path: "https://dl.dropbox.com/s/aker8plx4qg8tq6/12%20Tumi%20kathal.mp3?dl=1"
+  },
+  {
+    name: "তোমার উম্মত কেমন আছে",
+    artist: "সন্দীপন শিল্পী গোষ্ঠী",
+    album: "রোদেলা দুপুর",
+    image: "../images/trail.jpg",
+    path: "https://dl.dropbox.com/s/ig43t8ti4kmj7v9/13%20Tomar%20ummat.mp3?dl=1"
+  },
+];
--- players/template.js
+++ players/template.js
@@ -1,114 +1,114 @@
 // Define the tracks that have to be played
 let track_list = [
   {
-    name: "হে খোদা মোর হৃদয় হতে",
-    artist: "মতিউর রহমান মল্লিক",
-    album: "অ্যালবাম: প্রতীতি - ০১",
+    name: "Name",
+    artist: "ARTIST",
+    album: "ALBUM",
     image: "../images/karakoram.jpg",
     path: "LINK"
   },
    {
-    name: "হে খোদা মোর হৃদয় হতে",
-    artist: "মতিউর রহমান মল্লিক",
-    album: "অ্যালবাম: প্রতীতি - ০১",
+    name: "Name",
+    artist: "ARTIST",
+    album: "ALBUM",
     image: "../images/karakoram.jpg",
     path: "LINK"
   },
   {
-    name: "হে খোদা মোর হৃদয় হতে",
-    artist: "মতিউর রহমান মল্লিক",
-    album: "অ্যালবাম: প্রতীতি - ০১",
+    name: "Name",
+    artist: "ARTIST",
+    album: "ALBUM",
     image: "../images/karakoram.jpg",
     path: "LINK"
   },
   {
-    name: "হে খোদা মোর হৃদয় হতে",
-    artist: "মতিউর রহমান মল্লিক",
-    album: "অ্যালবাম: প্রতীতি - ০১",
+    name: "Name",
+    artist: "ARTIST",
+    album: "ALBUM",
     image: "../images/karakoram.jpg",
     path: "LINK"
   },
   {
-    name: "হে খোদা মোর হৃদয় হতে",
-    artist: "মতিউর রহমান মল্লিক",
-    album: "অ্যালবাম: প্রতীতি - ০১",
+    name: "Name",
+    artist: "ARTIST",
+    album: "ALBUM",
     image: "../images/karakoram.jpg",
     path: "LINK"
   },
   {
-    name: "হে খোদা মোর হৃদয় হতে",
-    artist: "মতিউর রহমান মল্লিক",
-    album: "অ্যালবাম: প্রতীতি - ০১",
+    name: "Name",
+    artist: "ARTIST",
+    album: "ALBUM",
     image: "../images/karakoram.jpg",
     path: "LINK"
   },
    {
-    name: "হে খোদা মোর হৃদয় হতে",
-    artist: "মতিউর রহমান মল্লিক",
-    album: "অ্যালবাম: প্রতীতি - ০১",
+    name: "Name",
+    artist: "ARTIST",
+    album: "ALBUM",
     image: "../images/karakoram.jpg",
     path: "LINK"
   },
   {
-    name: "হে খোদা মোর হৃদয় হতে",
-    artist: "মতিউর রহমান মল্লিক",
-    album: "অ্যালবাম: প্রতীতি - ০১",
+    name: "Name",
+    artist: "ARTIST",
+    album: "ALBUM",
     image: "../images/karakoram.jpg",
     path: "LINK"
   },
    {
-    name: "হে খোদা মোর হৃদয় হতে",
-    artist: "মতিউর রহমান মল্লিক",
-    album: "অ্যালবাম: প্রতীতি - ০১",
+    name: "Name",
+    artist: "ARTIST",
+    album: "ALBUM",
     image: "../images/karakoram.jpg",
     path: "LINK"
   },
   {
-    name: "হে খোদা মোর হৃদয় হতে",
-    artist: "মতিউর রহমান মল্লিক",
-    album: "অ্যালবাম: প্রতীতি - ০১",
+    name: "Name",
+    artist: "ARTIST",
+    album: "ALBUM",
     image: "../images/karakoram.jpg",
     path: "LINK"
   },
    {
-    name: "হে খোদা মোর হৃদয় হতে",
-    artist: "মতিউর রহমান মল্লিক",
-    album: "অ্যালবাম: প্রতীতি - ০১",
+    name: "Name",
+    artist: "ARTIST",
+    album: "ALBUM",
     image: "../images/karakoram.jpg",
     path: "LINK"
   },
   {
-    name: "হে খোদা মোর হৃদয় হতে",
-    artist: "মতিউর রহমান মল্লিক",
-    album: "অ্যালবাম: প্রতীতি - ০১",
+    name: "Name",
+    artist: "ARTIST",
+    album: "ALBUM",
     image: "../images/karakoram.jpg",
     path: "LINK"
   },
    {
-    name: "হে খোদা মোর হৃদয় হতে",
-    artist: "মতিউর রহমান মল্লিক",
-    album: "অ্যালবাম: প্রতীতি - ০১",
+    name: "Name",
+    artist: "ARTIST",
+    album: "ALBUM",
     image: "../images/karakoram.jpg",
     path: "LINK"
   },
   {
-    name: "হে খোদা মোর হৃদয় হতে",
-    artist: "মতিউর রহমান মল্লিক",
-    album: "অ্যালবাম: প্রতীতি - ০১",
+    name: "Name",
+    artist: "ARTIST",
+    album: "ALBUM",
     image: "../images/karakoram.jpg",
     path: "LINK"
   },
    {
-    name: "হে খোদা মোর হৃদয় হতে",
-    artist: "মতিউর রহমান মল্লিক",
-    album: "অ্যালবাম: প্রতীতি - ০১",
+    name: "Name",
+    artist: "ARTIST",
+    album: "ALBUM",
     image: "../images/karakoram.jpg",
     path: "LINK"
   },
   {
-    name: "হে খোদা মোর হৃদয় হতে",
-    artist: "মতিউর রহমান মল্লিক",
-    album: "অ্যালবাম: প্রতীতি - ০১",
+    name: "Name",
+    artist: "ARTIST",
+    album: "ALBUM",
     image: "../images/karakoram.jpg",
     path: "LINK"
   },
