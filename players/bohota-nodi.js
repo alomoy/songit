8487ddf2084345edd4c84679198b5230436b003a
alomoy <alomoyinfo@gmail.com>
2024-01-14 12:34:30 +0600

    Add bohota nodi

---
+++ players/bohota-nodi.js
@@ -0,0 +1,108 @@
+// Define the tracks that have to be played
+let track_list = [
+  {
+    name: "বহতা নদীর মতো",
+    artist: "সাইফুল্লাহ মানছুরমানছুর",
+    album: "বহতা নদী",
+    image: "../images/river.jpg",
+    path: "https://www.dropbox.com/s/2698ws9v29pb6ky/1bohota%20nodir.mp3?dl=1"
+  },
+   {
+    name: "জ্বলছে কাশ্মীর",
+    artist: "সাইফুল্লাহ মানছুরমানছুর",
+    album: "বহতা নদী",
+    image: "../images/nature.jpg",
+    path: "https://www.dropbox.com/s/weugb199y7rkbsz/2jwolche%20kasmir.mp3?dl=1"
+  },
+  {
+    name: "ঘুম থেকে জেগে দেখি",
+    artist: "সাইফুল্লাহ মানছুরমানছুর",
+    album: "বহতা নদী",
+    image: "../images/trail.jpg",
+    path: "https://www.dropbox.com/s/ni4id1nlhvs1hlx/3ghum%20theke%20jege.mp3?dl=1"
+  },
+  {
+    name: "আছো তুমি হৃদয় জুড়ে",
+    artist: "সাইফুল্লাহ মানছুরমানছুর",
+    album: "বহতা নদী",
+    image: "../images/karakoram.jpg",
+    path: "https://www.dropbox.com/s/8mdz2sclkoae0vo/4acho%20tumi.mp3?dl=1"
+  },
+  {
+    name: "এক হও মুসলিম এক হও আজ",
+    artist: "সাইফুল্লাহ মানছুরমানছুর",
+    album: "বহতা নদী",
+    image: "../images/hillroad.jpg",
+    path: "https://www.dropbox.com/s/he5pbzwa15be2ub/5ek%20hau%20muslim.mp3?dl=1"
+  },
+  {
+    name: "যার স্নেহের কোনো",
+    artist: "সাইফুল্লাহ মানছুরমানছুর",
+    album: "বহতা নদী",
+    image: "../images/mtroad.jpg",
+    path: "https://www.dropbox.com/s/9z5sksrhr8q2s4y/6zar%20sneher.mp3?dl=1"
+  },
+   {
+    name: "দুঃখের বানে বিদ্ধ",
+    artist: "সাইফুল্লাহ মানছুরমানছুর",
+    album: "বহতা নদী",
+    image: "../images/tunnel.jpg",
+    path: "https://www.dropbox.com/s/a6xxzk0bn0g60yo/7dukher%20bane%20biddho.mp3?dl=1"
+  },
+  {
+    name: "যে কোনো কাজ",
+    artist: "সাইফুল্লাহ মানছুরমানছুর",
+    album: "বহতা নদী",
+    image: "../images/train.jpg",
+    path: "https://www.dropbox.com/s/o1ixjsdpxjrecm0/8ze%20kOno%20kaj.mp3?dl=1"
+  },
+   {
+    name: "হাত দুটি সাদা",
+    artist: "সাইফুল্লাহ মানছুরমানছুর",
+    album: "বহতা নদী",
+    image: "../images/sajek.jpg",
+    path: "https://www.dropbox.com/s/6mqtqbquqd1aqdg/9hat%20duti%20sada.mp3?dl=1"
+  },
+  {
+    name: "Name",
+    artist: "সাইফুল্লাহ মানছুরমানছুর",
+    album: "বহতা নদী",
+    image: "../images/nature.jpg",
+    path: "https://www.dropbox.com/s/tyutmidg89p0one/10pother%20thikana.mp3?dl=1"
+  },
+   {
+    name: "পথের ঠিকানা",
+    artist: "সাইফুল্লাহ মানছুরমানছুর",
+    album: "বহতা নদী",
+    image: "../images/mtroad.jpg",
+    path: "https://www.dropbox.com/s/nnrt3gfw8cyk04z/11prithibite%20keu.mp3?dl=1"
+  },
+  {
+    name: "পৃথিবীতে কেউ স্থায়ী হয় না",
+    artist: "সাইফুল্লাহ মানছুরমানছুর",
+    album: "বহতা নদী",
+    image: "../images/mount.jpg",
+    path: "https://www.dropbox.com/s/b5lwwgioq86am0c/12rat%20keteche.mp3?dl=1"
+  },
+   {
+    name: "রাত কেটেছে স্বপ্নে বিভোর",
+    artist: "সাইফুল্লাহ মানছুরমানছুর",
+    album: "বহতা নদী",
+    image: "../images/mtroad.jpg",
+    path: "LINK"
+  },
+  {
+    name: "মায়ের জন্যে ফেরদাউস",
+    artist: "সাইফুল্লাহ মানছুরমানছুর",
+    album: "বহতা নদী",
+    image: "../images/nature.jpg",
+    path: "https://www.dropbox.com/s/qr6mmkmt87ipgfb/13mayer%20jonyo%20ferdaus.mp3?dl=1"
+  },
+   {
+    name: "আমাদের এক করে দাও",
+    artist: "সাইফুল্লাহ মানছুরমানছুর",
+    album: "বহতা নদী",
+    image: "../images/sajek.jpg",
+    path: "https://www.dropbox.com/s/6yy20taom9o2zkg/14amader%20ek%20kore.mp3?dl=1"
+  },
+];
--- players/template.js
+++ players/template.js
@@ -4,14 +4,21 @@ let track_list = [
     name: "Name",
     artist: "ARTIST",
     album: "ALBUM",
-    image: "../images/karakoram.jpg",
+    image: "../images/mount.jpg",
     path: "LINK"
   },
    {
     name: "Name",
     artist: "ARTIST",
     album: "ALBUM",
-    image: "../images/karakoram.jpg",
+    image: "../images/nature.jpg",
+    path: "LINK"
+  },
+  {
+    name: "Name",
+    artist: "ARTIST",
+    album: "ALBUM",
+    image: "../images/trail.jpg",
     path: "LINK"
   },
   {
@@ -25,91 +32,84 @@ let track_list = [
     name: "Name",
     artist: "ARTIST",
     album: "ALBUM",
-    image: "../images/karakoram.jpg",
+    image: "../images/hillroad.jpg",
     path: "LINK"
   },
   {
     name: "Name",
     artist: "ARTIST",
     album: "ALBUM",
-    image: "../images/karakoram.jpg",
-    path: "LINK"
-  },
-  {
-    name: "Name",
-    artist: "ARTIST",
-    album: "ALBUM",
-    image: "../images/karakoram.jpg",
+    image: "../images/mtroad.jpg",
     path: "LINK"
   },
    {
     name: "Name",
     artist: "ARTIST",
     album: "ALBUM",
-    image: "../images/karakoram.jpg",
+    image: "../images/tunnel.jpg",
     path: "LINK"
   },
   {
     name: "Name",
     artist: "ARTIST",
     album: "ALBUM",
-    image: "../images/karakoram.jpg",
+    image: "../images/train.jpg",
     path: "LINK"
   },
    {
     name: "Name",
     artist: "ARTIST",
     album: "ALBUM",
-    image: "../images/karakoram.jpg",
+    image: "../images/sajek.jpg",
     path: "LINK"
   },
   {
     name: "Name",
     artist: "ARTIST",
     album: "ALBUM",
-    image: "../images/karakoram.jpg",
+    image: "../images/nature.jpg",
     path: "LINK"
   },
    {
     name: "Name",
     artist: "ARTIST",
     album: "ALBUM",
-    image: "../images/karakoram.jpg",
+    image: "../images/mtroad.jpg",
     path: "LINK"
   },
   {
     name: "Name",
     artist: "ARTIST",
     album: "ALBUM",
-    image: "../images/karakoram.jpg",
+    image: "../images/mount.jpg",
     path: "LINK"
   },
    {
     name: "Name",
     artist: "ARTIST",
     album: "ALBUM",
-    image: "../images/karakoram.jpg",
+    image: "../images/mtroad.jpg",
     path: "LINK"
   },
   {
     name: "Name",
     artist: "ARTIST",
     album: "ALBUM",
-    image: "../images/karakoram.jpg",
+    image: "../images/nature.jpg",
     path: "LINK"
   },
    {
     name: "Name",
     artist: "ARTIST",
     album: "ALBUM",
-    image: "../images/karakoram.jpg",
+    image: "../images/sajek.jpg",
     path: "LINK"
   },
   {
     name: "Name",
     artist: "ARTIST",
     album: "ALBUM",
-    image: "../images/karakoram.jpg",
+    image: "../images/laptop.jpg",
     path: "LINK"
   },
 ];
