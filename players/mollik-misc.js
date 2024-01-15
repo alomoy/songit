03754800471a41ba1f83c3c12bd3066bece94ba3
alomoy <alomoyinfo@gmail.com>
2024-01-15 13:18:25 +0600

    Add mollik-misc

---
+++ players/mollik-misc.js
@@ -0,0 +1,80 @@
+// Define the tracks that have to be played
+let track_list = [
+   {
+    name: "আমাকে দাও সে ঈমান",
+    artist: "মতিউর রহমান মল্লিক",
+    album: "গুচ্ছ - ০১",
+    image: "../images/tunnel.jpg",
+    path: "https://www.dropbox.com/scl/fi/tn1z1g0j04erzqjkup8xt/amake-dao-sei.mp3?rlkey=0xycr1gcxk3kjqtkhxulmskob&dl=1"
+  },
+  {
+    name: "চলো মুজাহিদ জিহাদে",
+    artist: "মতিউর রহমান মল্লিক",
+    album: "গুচ্ছ - ০১",
+    image: "../images/mount.jpg",
+    path: "https://www.dropbox.com/scl/fi/5ih8vq72bdoviy64ggv3n/cholo-cholo.mp3?rlkey=t6vt2tv2pgv91gyxeaklhuc83&dl=1	"
+  },
+   {
+    name: "এই গুনাহগার",
+    artist: "মতিউর রহমান মল্লিক",
+    album: "গুচ্ছ - ০১",
+    image: "../images/nature.jpg",
+    path: "https://www.dropbox.com/scl/fi/j79k0kze2p3l97y0g3pze/ei-gunahgar.mp3?rlkey=c2lusbhg5bwsmedr01z0h2bsi&dl=1"
+  },
+  {
+    name: "ঈমানের দাবী যদি",
+    artist: "মতিউর রহমান মল্লিক",
+    album: "গুচ্ছ - ০১",
+    image: "../images/trail.jpg",
+    path: "https://www.dropbox.com/scl/fi/0eo1rbrkdqs7ur0ernzzv/Emaner-dabi.mp3?rlkey=yvxcej20lh5h4dlw9nidbfr6t&dl=1"
+  },
+  {
+    name: "Name",
+    artist: "মতিউর রহমান মল্লিক",
+    album: "গুচ্ছ - ০১",
+    image: "../images/karakoram.jpg",
+    path: "https://www.dropbox.com/scl/fi/k7wz54u51jec6ncjqqbf2/Jihad-korte.mp3?rlkey=72pyab1asntdgkflb4kvdnuxs&dl=1"
+  },
+  {
+    name: "Name",
+    artist: "মতিউর রহমান মল্লিক",
+    album: "গুচ্ছ - ০১",
+    image: "../images/hillroad.jpg",
+    path: "https://www.dropbox.com/scl/fi/hqml7rq5iw7sb3ei0wjym/jihader-mane-holo.mp3?rlkey=pm0tnih8o3o234gjffxcc4avp&dl=1"
+  },
+  {
+    name: "Name",
+    artist: "মতিউর রহমান মল্লিক",
+    album: "গুচ্ছ - ০১",
+    image: "../images/mtroad.jpg",
+    path: "https://www.dropbox.com/scl/fi/cus14dkaaea9nixfsef3q/Rasul-amar-alo-asha.mp3?rlkey=v75oy8dpnha74sbduadx80icv&dl=1"
+  },
+   {
+    name: "Name",
+    artist: "মতিউর রহমান মল্লিক",
+    album: "গুচ্ছ - ০১",
+    image: "../images/tunnel.jpg",
+    path: "https://www.dropbox.com/scl/fi/p22otwmmixo8lcxko7tb5/shohorer-futpathe.mp3?rlkey=ph2a0x8k69x5pmmpcpk9qpmvb&dl=1"
+  },
+  {
+    name: "Name",
+    artist: "মতিউর রহমান মল্লিক",
+    album: "গুচ্ছ - ০১",
+    image: "../images/train.jpg",
+    path: "https://www.dropbox.com/scl/fi/be49u1bd83r3b6pg6uvbl/shundor-shundor-bon.mp3?rlkey=0zfurnzs6bt6exbks2jist3hl&dl=1"
+  },
+   {
+    name: "Name",
+    artist: "মতিউর রহমান মল্লিক",
+    album: "গুচ্ছ - ০১",
+    image: "../images/sajek.jpg",
+    path: "https://www.dropbox.com/scl/fi/qis4tt1aap3dw0inareky/tik-tik-je-ghorita.mp3?rlkey=pzvd7cgeb3s6ipzr1hegzrl1m&dl=1"
+  },
+  {
+    name: "Name",
+    artist: "মতিউর রহমান মল্লিক",
+    album: "গুচ্ছ - ০১",
+    image: "../images/nature.jpg",
+    path: "https://www.dropbox.com/scl/fi/y6lx8a84a2q8qwjr4bukt/tomar-name-jodi.mp3?rlkey=adwvds06fqb2mza85kn80nrfs&dl=1"
+  },
+];
