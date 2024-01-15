de00c8b1353a178e9c4f652bc424ce84a7213f3e
alomoy <alomoyinfo@gmail.com>
2024-01-15 13:24:27 +0600

    add mollik misc song names

--- players/mollik-misc.js
+++ players/mollik-misc.js
@@ -29,49 +29,49 @@ let track_list = [
     path: "https://www.dropbox.com/scl/fi/0eo1rbrkdqs7ur0ernzzv/Emaner-dabi.mp3?rlkey=yvxcej20lh5h4dlw9nidbfr6t&dl=1"
   },
   {
-    name: "Name",
+    name: "জিহাদ করতে চাই",
     artist: "মতিউর রহমান মল্লিক",
     album: "গুচ্ছ - ০১",
     image: "../images/karakoram.jpg",
     path: "https://www.dropbox.com/scl/fi/k7wz54u51jec6ncjqqbf2/Jihad-korte.mp3?rlkey=72pyab1asntdgkflb4kvdnuxs&dl=1"
   },
   {
-    name: "Name",
+    name: "জিহাদের মানে হলো",
     artist: "মতিউর রহমান মল্লিক",
     album: "গুচ্ছ - ০১",
     image: "../images/hillroad.jpg",
     path: "https://www.dropbox.com/scl/fi/hqml7rq5iw7sb3ei0wjym/jihader-mane-holo.mp3?rlkey=pm0tnih8o3o234gjffxcc4avp&dl=1"
   },
   {
-    name: "Name",
+    name: "রাসুল আমার আলো আশা",
     artist: "মতিউর রহমান মল্লিক",
     album: "গুচ্ছ - ০১",
     image: "../images/mtroad.jpg",
     path: "https://www.dropbox.com/scl/fi/cus14dkaaea9nixfsef3q/Rasul-amar-alo-asha.mp3?rlkey=v75oy8dpnha74sbduadx80icv&dl=1"
   },
    {
-    name: "Name",
+    name: "শহরের ফুটপাথে",
     artist: "মতিউর রহমান মল্লিক",
     album: "গুচ্ছ - ০১",
     image: "../images/tunnel.jpg",
     path: "https://www.dropbox.com/scl/fi/p22otwmmixo8lcxko7tb5/shohorer-futpathe.mp3?rlkey=ph2a0x8k69x5pmmpcpk9qpmvb&dl=1"
   },
   {
-    name: "Name",
+    name: "সুন্দর সুন্দর বন যে",
     artist: "মতিউর রহমান মল্লিক",
     album: "গুচ্ছ - ০১",
     image: "../images/train.jpg",
     path: "https://www.dropbox.com/scl/fi/be49u1bd83r3b6pg6uvbl/shundor-shundor-bon.mp3?rlkey=0zfurnzs6bt6exbks2jist3hl&dl=1"
   },
    {
-    name: "Name",
+    name: "টিক টিক যে ঘড়িটা",
     artist: "মতিউর রহমান মল্লিক",
     album: "গুচ্ছ - ০১",
     image: "../images/sajek.jpg",
     path: "https://www.dropbox.com/scl/fi/qis4tt1aap3dw0inareky/tik-tik-je-ghorita.mp3?rlkey=pzvd7cgeb3s6ipzr1hegzrl1m&dl=1"
   },
   {
-    name: "Name",
+    name: "তোমার নামে যদি গান",
     artist: "মতিউর রহমান মল্লিক",
     album: "গুচ্ছ - ০১",
     image: "../images/nature.jpg",
