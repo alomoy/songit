5d70bd15ceb4ea9ed0b3826337c3a55639a3f93d
alomoy <alomoyinfo@gmail.com>
2024-01-16 09:46:53 +0600

    Add chiro omlan

---
+++ players/chiro-omlan.js
@@ -0,0 +1,73 @@
+// Define the tracks that have to be played
+let track_list = [
+  {
+    name: "আমরা পৃথিবীর যেখানে",
+    artist: "সাইফুল্লাহ মানছুর",
+    album: "চির অম্লান",
+    image: "../images/mount.jpg",
+    path: "https://www.dropbox.com/scl/fi/lupcyej8y7gjm83h18gz5/01.amra-prithibir-zekhane-thaki.mp3?rlkey=qzx0x0by8c8h4obfuej1i5q9u&dl=1"
+  },
+   {
+    name: "ইয়া আল্লাহ ইয়া রাহমান",
+    artist: "সাইফুল্লাহ মানছুর",
+    album: "চির অম্লান",
+    image: "../images/nature.jpg",
+    path: "https://www.dropbox.com/scl/fi/y53rq89x9ufo5s84nwi2c/02.ya-allah-ya-rahman.mp3?rlkey=b42e4t144l3cnkgjgge0m3vri&dl=1"
+  },
+  {
+    name: "আল্লাহ যখন কাউকে",
+    artist: "সাইফুল্লাহ মানছুর",
+    album: "চির অম্লান",
+    image: "../images/trail.jpg",
+    path: "https://www.dropbox.com/scl/fi/fd55zb24miri6vo3nimkj/03.allah-zokhon-kauke-dhoren.mp3?rlkey=9lqh15ed0u41fhlhkz39dhbb2&dl=1"
+  },
+  {
+    name: "রাসুলুল্লাহ নবিউল্লাহ",
+    artist: "সাইফুল্লাহ মানছুর",
+    album: "চির অম্লান",
+    image: "../images/karakoram.jpg",
+    path: "https://www.dropbox.com/scl/fi/onea6ccjaerwlvknwoalq/04.rasulullah-nabiullah.mp3?rlkey=7cpuihw7t7232grx3ghxa4w33&dl=1"
+  },
+  {
+    name: "সময় তো বয়ে যায়",
+    artist: "সাইফুল্লাহ মানছুর",
+    album: "চির অম্লান",
+    image: "../images/time.jpg",
+    path: "https://www.dropbox.com/scl/fi/hc2rqm9rb886i89j0ebam/05.somoy-to-boye-boye-zay.mp3?rlkey=0q5uyvu6sg1tkdxnyj9ebtjjh&dl=1"
+  },
+  {
+    name: "আমার আজও ডাক আসেনি",
+    artist: "সাইফুল্লাহ মানছুর",
+    album: "চির অম্লান",
+    image: "../images/mtroad.jpg",
+    path: "https://www.dropbox.com/scl/fi/y6d6w3yor1d6zie8cro5i/06.amar-ajo-dak-aseni.mp3?rlkey=xkcj05xwxms0ldgi4mala7oha&dl=1"
+  },
+   {
+    name: "আমাদের এই দুটি খালি হাত",
+    artist: "সাইফুল্লাহ মানছুর",
+    album: "চির অম্লান",
+    image: "../images/tunnel.jpg",
+    path: "https://www.dropbox.com/scl/fi/auv13i4epm0kpveumhzsc/07.amader-ai-duti-khali-hat.mp3?rlkey=tde0ciqa9fa3cupn2l0c5l063&dl=1"
+  },
+  {
+    name: "তোমারই গান গায় পাখি",
+    artist: "সাইফুল্লাহ মানছুর",
+    album: "চির অম্লান",
+    image: "../images/train.jpg",
+    path: "https://www.dropbox.com/scl/fi/ws33b4huegxoz7ykb0u82/08.tomari-name-gay-pakhira-gan.mp3?rlkey=jhx8sgu4fdvav0brm9yluy52j&dl=1"
+  },
+   {
+    name: "আবার আসবে ফিরে",
+    artist: "সাইফুল্লাহ মানছুর",
+    album: "চির অম্লান",
+    image: "../images/sajek.jpg",
+    path: "https://www.dropbox.com/scl/fi/aj8olhh89o10crdasc369/09.abar-asbe-fire.mp3?rlkey=j5txktyofpo7g5djpvyaqx3po&dl=1"
+  },
+  {
+    name: "বাবা তুমি চলে গেছো",
+    artist: "সাইফুল্লাহ মানছুর",
+    album: "চির অম্লান",
+    image: "../images/nature.jpg",
+    path: "https://www.dropbox.com/scl/fi/f6set29dq3orchs27eg35/10.baba-tumi-chole-gecho.mp3?rlkey=pt0vzf6mklqr38rld8upgza5q&dl=1"
+  },
+];
