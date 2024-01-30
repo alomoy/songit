cd61ce4a5e88eee3c8e38937bb529b580654ffca
alomoy <alomoyinfo@gmail.com>
2024-01-30 23:49:58 +0600

    Add pahari jhorna

---
+++ players/pahari-jhorna.js
@@ -0,0 +1,94 @@
+// Define the tracks that have to be played
+let track_list = [
+  {
+    name: "কার নামে ভেসে",
+    artist: "টাইফুন শিল্পীগোষ্ঠী",
+    album: "পাহাড়ী ঝরণা",
+    image: "../images/mount.jpg",
+    path: "https://dl.dropbox.com/s/ae2qwg52eynnti7/001-Kar%20Nam.mp3"
+  },
+   {
+    name: "এই উদাসী মন",
+    artist: "টাইফুন শিল্পীগোষ্ঠী",
+    album: "পাহাড়ী ঝরণা",
+    image: "../images/nature.jpg",
+    path: "https://dl.dropbox.com/s/t192xq589terzxa/002-Ei%20Udashi%20Mon.mp3"
+  },
+  {
+    name: "ও নদী একটু শোনো",
+    artist: "টাইফুন শিল্পীগোষ্ঠী",
+    album: "পাহাড়ী ঝরণা",
+    image: "../images/trail.jpg",
+    path: "https://dl.dropbox.com/s/pd1n29h5sptnexo/005-O%20Nodi%20Ektu%20Shono.mp3"
+  },
+  {
+    name: "নিও না নিও না প্রভূ ",
+    artist: "টাইফুন শিল্পীগোষ্ঠী",
+    album: "পাহাড়ী ঝরণা",
+    image: "../images/karakoram.jpg",
+    path: "https://dl.dropbox.com/s/cg9ek00t7lza2qw/004-Niyona%20Niyona%20Provu.mp3"
+  },
+  {
+    name: "ও.. গন্ধরাজ",
+    artist: "টাইফুন শিল্পীগোষ্ঠী",
+    album: "পাহাড়ী ঝরণা",
+    image: "../images/hillroad.jpg",
+    path: "https://dl.dropbox.com/s/9azlxl2wgb487ii/003-O%20Gondhoraj.mp3"
+  },
+  {
+    name: "আকাশে তারা হাসে",
+    artist: "টাইফুন শিল্পীগোষ্ঠী",
+    album: "পাহাড়ী ঝরণা",
+    image: "../images/mtroad.jpg",
+    path: "https://www.dropbox.com/s/k54r9d1nhn0gi61/006-Akashe%20Tara%20Hashe.mp3?dl=1"
+  },
+   {
+    name: "আল্লাহু আল্লাহু ",
+    artist: "টাইফুন শিল্পীগোষ্ঠী",
+    album: "পাহাড়ী ঝরণা",
+    image: "../images/tunnel.jpg",
+    path: "https://www.dropbox.com/s/h4rvp2xfhosnks6/007-Allahu%20Allahu.mp3?dl=1"
+  },
+  {
+    name: "তোমার স্বর্গ তোমার",
+    artist: "টাইফুন শিল্পীগোষ্ঠী",
+    album: "পাহাড়ী ঝরণা",
+    image: "../images/train.jpg",
+    path: "https://www.dropbox.com/s/r52t2xxuojof7wr/008-Tomar%20Shorgo.mp3?dl=1"
+  },
+   {
+    name: "ও ফুল পাখি",
+    artist: "টাইফুন শিল্পীগোষ্ঠী",
+    album: "পাহাড়ী ঝরণা",
+    image: "../images/sajek.jpg",
+    path: "https://www.dropbox.com/s/fm9s6rmlzn567y9/009-O%20Ful%20Pakhi.mp3?dl=1"
+  },
+  {
+    name: "প্রিয় দেশ",
+    artist: "টাইফুন শিল্পীগোষ্ঠী",
+    album: "পাহাড়ী ঝরণা",
+    image: "../images/nature.jpg",
+    path: "https://www.dropbox.com/s/ce24af6w445tkhv/010-Priyo%20Desh.mp3?dl=1"
+  },
+   {
+    name: "কোথায় আছো মা জননী",
+    artist: "টাইফুন শিল্পীগোষ্ঠী",
+    album: "পাহাড়ী ঝরণা",
+    image: "../images/mtroad.jpg",
+    path: "https://www.dropbox.com/s/7byf8w5o4124a03/011-Kothay%20Acho%20Ma.mp3?dl=1"
+  },
+  {
+    name: "আকাশ জুড়ে",
+    artist: "টাইফুন শিল্পীগোষ্ঠী",
+    album: "পাহাড়ী ঝরণা",
+    image: "../images/mount.jpg",
+    path: "https://www.dropbox.com/s/2mbxdwzq45vytcc/012-Akash%20Jure.mp3?dl=1"
+  },
+   {
+    name: "মাকে দেখি না",
+    artist: "টাইফুন শিল্পীগোষ্ঠী",
+    album: "পাহাড়ী ঝরণা",
+    image: "../images/mosque.jpg",
+    path: "https://www.dropbox.com/s/mytzsdktnsk2ihj/013-Make%20Dekhi%20Na.mp3?dl=1"
+  },
+];
