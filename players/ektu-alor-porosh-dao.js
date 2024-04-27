aae08e4837bd3f19bc344000fea735682b137c95
alomoy <alomoyinfo@gmail.com>
2024-04-27 06:21:51 +0600

    Add ektu alor porosh dao

---
+++ players/ektu-alor-porosh-dao.js
@@ -0,0 +1,101 @@
+// Define the tracks that have to be played
+let track_list = [
+  {
+    name: "তোমার ভালবাসা",
+    artist: "ওবায়দুল্লাহ তারেক",
+    album: "একটু আলোর পরশ দাও",
+    image: "../images/album/ektu-alor-porosh-dao.jpg",
+    path: "https://dl.dropbox.com/s/ttw649pzylyxbnx/1tomar%20valobasa.mp3?dl=1"
+  },
+   {
+    name: "রাসুল তুমি আমার",
+    artist: "ওবায়দুল্লাহ তারেক",
+    album: "একটু আলোর পরশ দাও",
+    image: "../images/nature.jpg",
+    path: "https://dl.dropbox.com/s/4ws1sadaa7k9hro/2rasul%20tumi%20amar.mp3?dl=1"
+  },
+  {
+    name: "মাটির এক খান ঘর",
+    artist: "ওবায়দুল্লাহ তারেক",
+    album: "একটু আলোর পরশ দাও",
+    image: "../images/trail.jpg",
+    path: "https://dl.dropbox.com/s/l6tnnf1jr3ywrlk/3matir%20ek%20khana.mp3?dl=1"
+  },
+  {
+    name: "মা তোমার স্মৃতি",
+    artist: "ওবায়দুল্লাহ তারেক",
+    album: "একটু আলোর পরশ দাও",
+    image: "../images/karakoram.jpg",
+    path: "https://dl.dropbox.com/s/2jnl0u67tsdli38/4ma%20tomar%20smriti%20vora.mp3?dl=1"
+  },
+  {
+    name: "একদিন পরপারে ভাসাবিরে নাও",
+    artist: "ওবায়দুল্লাহ তারেক",
+    album: "একটু আলোর পরশ দাও",
+    image: "../images/hillroad.jpg",
+    path: "https://dl.dropbox.com/s/umysspj51mswpzj/5ek%20din%20poropare.mp3?dl=1"
+  },
+  {
+    name: "নদী আমার যায়রে",
+    artist: "ওবায়দুল্লাহ তারেক",
+    album: "একটু আলোর পরশ দাও",
+    image: "../images/mtroad.jpg",
+    path: "https://dl.dropbox.com/s/1hco6bmdzollkiu/6nodi%20amar%20zayre.mp3?dl=1"
+  },
+   {
+    name: "ও পরানের পাখি",
+    artist: "ওবায়দুল্লাহ তারেক",
+    album: "একটু আলোর পরশ দাও",
+    image: "../images/tunnel.jpg",
+    path: "https://dl.dropbox.com/s/wbixpgbxsy8lqny/7o%20poraner%20pakhi.mp3?dl=1"
+  },
+  {
+    name: "একটু আলোর পরশ দাও",
+    artist: "ওবায়দুল্লাহ তারেক",
+    album: "একটু আলোর পরশ দাও",
+    image: "../images/train.jpg",
+    path: "https://dl.dropbox.com/s/roe98ctd2i0zww9/8alor%20ektu%20porosh%20dao.mp3?dl=1"
+  },
+   {
+    name: "আমি অশ্রু জলে",
+    artist: "ওবায়দুল্লাহ তারেক",
+    album: "একটু আলোর পরশ দাও",
+    image: "../images/sajek.jpg",
+    path: "https://dl.dropbox.com/s/l34pyop119rm9ba/9ami%20oshru%20jole.mp3?dl=1"
+  },
+  {
+    name: "ও মন তুমি",
+    artist: "ওবায়দুল্লাহ তারেক",
+    album: "একটু আলোর পরশ দাও",
+    image: "../images/nature.jpg",
+    path: "https://dl.dropbox.com/s/hqxnbyevoh9ls3s/10o%20mon%20tumi.mp3?dl=1"
+  },
+   {
+    name: "মাওলা তোমার লাগিয়া",
+    artist: "ওবায়দুল্লাহ তারেক",
+    album: "একটু আলোর পরশ দাও",
+    image: "../images/allahu.jpg",
+    path: "https://dl.dropbox.com/s/zxk6wu4acfsqj0h/11maola%20toamr%20lagiya.mp3?dl=1"
+  },
+  {
+    name: "স্বাধীনতা আহ",
+    artist: "ওবায়দুল্লাহ তারেক",
+    album: "একটু আলোর পরশ দাও",
+    image: "../images/mount.jpg",
+    path: "https://dl.dropbox.com/s/u5wenqzwgbmt4rv/12swadhinota%20ah%20swadhinota.mp3?dl=1"
+  },
+   {
+    name: "নদী-ই-নদী",
+    artist: "ওবায়দুল্লাহ তারেক",
+    album: "একটু আলোর পরশ দাও",
+    image: "../images/mosque.jpg",
+    path: "https://dl.dropbox.com/s/zqpsn7ccvanadtq/13nodi%20e%20nodi.mp3?dl=1"
+  },
+  {
+    name: "আজকের এ সুর",
+    artist: "ওবায়দুল্লাহ তারেক",
+    album: "একটু আলোর পরশ দাও",
+    image: "../images/nature.jpg",
+    path: "https://dl.dropbox.com/s/0evmrwu4nplkwtb/14ajker%20e%20sur.mp3?dl=1"
+  },
+];
--- players/hijol-boner-pakhi.js
+++ players/hijol-boner-pakhi.js
@@ -2,105 +2,105 @@
 let track_list = [
   {
     name: "বাগানে কত ফুল",
-    artist: "হাসনাহেনা আফরিন",
+    artist: "হাসনা হেনা আফরীন",
     album: "হিজল বনের পাখি",
-    image: "../images/garden.jpg",
+    image: "../images/album/hijol-boner-pakhi.jpg",
     path: "https://www.dropbox.com/scl/fi/mtq04rt7menxhkdybubjs/1-bagane-koto-koto.mp3?rlkey=wu01fmqvbd0elgtdx0ogozf5j&st=9krkq4oc&dl=1"
   },
    {
     name: "চালতা পাতার কাজ",
-    artist: "হাসনাহেনা আফরিন",
+    artist: "হাসনা হেনা আফরীন",
     album: "হিজল বনের পাখি",
     image: "../images/leaf.jpg",
     path: "https://www.dropbox.com/scl/fi/swjfu7o4imd15xy0953ye/2-chalta-patar.mp3?rlkey=slqif9ra30barzhxeshksalub&st=6bwu5w3h&dl=1"
   },
   {
     name: "ছোট পাখি ছোট তার",
-    artist: "হাসনাহেনা আফরিন",
+    artist: "হাসনা হেনা আফরীন",
     album: "হিজল বনের পাখি",
     image: "../images/birds.jpg",
     path: "https://www.dropbox.com/scl/fi/1dntk0ajowdmapwndaond/3-chhoto-pakhi-thote-tar.mp3?rlkey=z9fpp8wsnwgrk6obma8wrvl16&st=fr0h4uak&dl=1"
   },
   {
     name: "দিনটা যেন মোমের",
-    artist: "হাসনাহেনা আফরিন",
+    artist: "হাসনা হেনা আফরীন",
     album: "হিজল বনের পাখি",
     image: "../images/light.jpg",
     path: "https://www.dropbox.com/scl/fi/07fgrubw21qamdhrk9iqy/4-dinta-jeno-momer.mp3?rlkey=ltdcmc2e4trhxji6nkmoj4lil&st=1hnn40s9&dl=1"
   },
   {
     name: "হে মেহেরবান",
-    artist: "হাসনাহেনা আফরিন",
+    artist: "হাসনা হেনা আফরীন",
     album: "হিজল বনের পাখি",
-    image: "../images/hillroad.jpg",
+    image: "../images/allahu.jpg",
     path: "https://www.dropbox.com/scl/fi/i0bdn4so97jcbey2i4job/5-he-meherban.mp3?rlkey=ppmi3mglaank3oytcjw2mvq3g&st=ff7woalm&dl=1"
   },
   {
     name: "হলদে ডানার সেই পাখি",
-    artist: "হাসনাহেনা আফরিন",
+    artist: "হাসনা হেনা আফরীন",
     album: "হিজল বনের পাখি",
     image: "../images/birds.jpg",
     path: "https://www.dropbox.com/scl/fi/svlptat6uajhpgoyyi8rt/6-holde-danar.mp3?rlkey=05a9nw72lcgc7k1ie4w6i6q1z&st=5efbk48i&dl=1"
   },
    {
     name: "জাগ্রত ছাড়ো ঘুম",
-    artist: "হাসনাহেনা আফরিন",
+    artist: "হাসনা হেনা আফরীন",
     album: "হিজল বনের পাখি",
     image: "../images/tunnel.jpg",
     path: "https://www.dropbox.com/scl/fi/99bjx91w4b1qcngrxd7jo/7-jagroto-chharo-ghum.mp3?rlkey=mbhpqlrrycymy5nh7neha239m&st=tuhti7cj&dl=1"
   },
   {
     name: "কর্ণফুলী শোনো",
-    artist: "হাসনাহেনা আফরিন",
+    artist: "হাসনা হেনা আফরীন",
     album: "হিজল বনের পাখি",
     image: "../images/river.jpg",
     path: "https://www.dropbox.com/scl/fi/f9m8ka9evr50ssz5eupk2/8-kornophuli-kornophuli.mp3?rlkey=m6zoveghcries67y24cr3pqsl&st=ex9pjc03&dl=1"
   },
    {
     name: "নীল ছাতাটা আকাশ",
-    artist: "হাসনাহেনা আফরিন",
+    artist: "হাসনা হেনা আফরীন",
     album: "হিজল বনের পাখি",
-    image: "../images/sajek.jpg",
+    image: "../images/nature.jpg",
     path: "https://www.dropbox.com/scl/fi/rximxnuhbiu9uoykkt0sa/9-nil-chhatata.mp3?rlkey=x3o6hqwwrtp0xpdrahvq3c8m8&st=dzt9hfx4&dl=1"
   },
   {
     name: "নদীরে তুই",
-    artist: "হাসনাহেনা আফরিন",
+    artist: "হাসনা হেনা আফরীন",
     album: "হিজল বনের পাখি",
     image: "../images/river.jpg",
     path: "https://www.dropbox.com/scl/fi/2tzduc6bgahpmwho0zmx3/10-nodir-tire-bosot.mp3?rlkey=8800ahtliyt02hxfu0n8gt0ps&st=vro0ryak&dl=1"
   },
    {
     name: "ওরে নিশিগন্ধা",
-    artist: "হাসনাহেনা আফরিন",
+    artist: "হাসনা হেনা আফরীন",
     album: "হিজল বনের পাখি",
     image: "../images/flower.jpg",
     path: "https://www.dropbox.com/scl/fi/50velm3gh00u8he63dzu8/11-ore-nishigondha.mp3?rlkey=2n4bhpfurk84r6d37nseo20o6&st=bjcdtvg8&dl=1"
   },
   {
     name: "পদ্মা নদীর পাড়ের",
-    artist: "হাসনাহেনা আফরিন",
+    artist: "হাসনা হেনা আফরীন",
     album: "হিজল বনের পাখি",
     image: "../images/river2.jpg",
     path: "https://www.dropbox.com/scl/fi/tg4520ph9rzowff75hq8h/12-padma-nodi-parer-moto.mp3?rlkey=8509ijcnh067nhm3hhsqm03sb&st=yukh0ndy&dl=1"
   },
    {
     name: "সাহস বুকে রাখো",
-    artist: "হাসনাহেনা আফরিন",
+    artist: "হাসনা হেনা আফরীন",
     album: "হিজল বনের পাখি",
     image: "../images/mosque.jpg",
     path: "https://www.dropbox.com/scl/fi/tg4520ph9rzowff75hq8h/12-padma-nodi-parer-moto.mp3?rlkey=8509ijcnh067nhm3hhsqm03sb&st=yukh0ndy&dl=1"
   },
   {
     name: "সালাতে বসল না মন",
-    artist: "হাসনাহেনা আফরিন",
+    artist: "হাসনা হেনা আফরীন",
     album: "হিজল বনের পাখি",
     image: "../images/nature.jpg",
     path: "https://www.dropbox.com/scl/fi/rfujpwz5o6l2cwkx4jgq0/14-salate-boslo-na-mon.mp3?rlkey=ho9zcui3ogpmqk9grp3kvzaq1&st=ctounqbx&dl=1"
   },
    {
     name: "সেই সংগ্রামী মানুষের",
-    artist: "হাসনাহেনা আফরিন",
+    artist: "হাসনা হেনা আফরীন",
     album: "হিজল বনের পাখি",
     image: "../images/horse.jpg",
     path: "https://www.dropbox.com/scl/fi/8sz17h0fopeifdrv64756/15-sei-songrami.mp3?rlkey=ywfu857bibhcpwsvg5sirh999&st=zquc4jr3&dl=1"
