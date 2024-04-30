e3d4d1fcc7aff86188eb896ddc12f15f7c0d65ea
alomoy <alomoyinfo@gmail.com>
2024-04-30 10:55:33 +0600

    Add kamini futeche bone

---
+++ players/kamini-futeche-bone.js
@@ -0,0 +1,108 @@
+// Define the tracks that have to be played
+let track_list = [
+  {
+    name: "কামিনী ফুটেছে",
+    artist: "মশিউর রহমান",
+    album: "কামিনী ফুটেছে বনে",
+    image: "../images/album/kamini-futeche-bone.jpg",
+    path: "https://dl.dropbox.com/s/6uosp5blm3tkn98/1kamini%20futeche.mp3?dl=0"
+  },
+   {
+    name: "মনের মাঝে",
+    artist: "মশিউর রহমান",
+    album: "কামিনী ফুটেছে বনে",
+    image: "../images/nature.jpg",
+    path: "https://dl.dropbox.com/s/qttxu6yv6sdqj2y/2moner%20maje.mp3?dl=0"
+  },
+  {
+    name: "পৃথিবীর হাজারো",
+    artist: "মশিউর রহমান",
+    album: "কামিনী ফুটেছে বনে",
+    image: "../images/trail.jpg",
+    path: "https://dl.dropbox.com/s/qhmxrhjx5dq2lmm/3prithibir%20hajaro.mp3?dl=0"
+  },
+  {
+    name: "যদি তিতুর বাঁশের",
+    artist: "মশিউর রহমান",
+    album: "কামিনী ফুটেছে বনে",
+    image: "../images/horse.jpg",
+    path: "https://dl.dropbox.com/s/901zh9uvu6pekfc/4zodi%20titur%20bansher.mp3?dl=0"
+  },
+  {
+    name: "যার এতায়াত করলে",
+    artist: "মশিউর রহমান",
+    album: "কামিনী ফুটেছে বনে",
+    image: "../images/hillroad.jpg",
+    path: "https://dl.dropbox.com/s/zaa1r6k3p7ucr4r/5zar%20etayat%20korle.mp3?dl=0"
+  },
+  {
+    name: "কোনো এক প্রভাতে",
+    artist: "মশিউর রহমান",
+    album: "কামিনী ফুটেছে বনে",
+    image: "../images/mosque.jpg",
+    path: "https://dl.dropbox.com/s/d7bld4r5mm5v0we/6kOn%20ek%20provate.mp3?dl=0"
+  },
+   {
+    name: "একটি গানের স্বরলিপি",
+    artist: "মশিউর রহমান",
+    album: "কামিনী ফুটেছে বনে",
+    image: "../images/song.png",
+    path: "https://dl.dropbox.com/s/266e1kiu75mph2x/7ekti%20ganer%20sworlipi.mp3?dl=0"
+  },
+  {
+    name: "যেন আল্লাহ নামে",
+    artist: "মশিউর রহমান",
+    album: "কামিনী ফুটেছে বনে",
+    image: "../images/train.jpg",
+    path: "https://dl.dropbox.com/s/f58jwn7hk4brk9f/8zen%20allahr%20name.mp3?dl=0"
+  },
+   {
+    name: "তুমি যদি কভু",
+    artist: "মশিউর রহমান",
+    album: "কামিনী ফুটেছে বনে",
+    image: "../images/sajek.jpg",
+    path: "https://dl.dropbox.com/s/cf5hlxdgh0bjbcv/9tumi%20zodi%20lovu.mp3?dl=0"
+  },
+  {
+    name: "হাজার স্বপ্ন ছিল",
+    artist: "মশিউর রহমান",
+    album: "কামিনী ফুটেছে বনে",
+    image: "../images/nature.jpg",
+    path: "https://dl.dropbox.com/s/tjthgfyux9uipr9/10hajar%20swopn%20chil.mp3?dl=0"
+  },
+   {
+    name: "কী কথা বলতে",
+    artist: "মশিউর রহমান",
+    album: "কামিনী ফুটেছে বনে",
+    image: "../images/mtroad.jpg",
+    path: "https://dl.dropbox.com/s/6qxyi7w4s33qk52/11ki%20kotha%20bolte.mp3?dl=0"
+  },
+  {
+    name: "রক্তে ভেসে যায়",
+    artist: "মশিউর রহমান",
+    album: "কামিনী ফুটেছে বনে",
+    image: "../images/mount.jpg",
+    path: "https://www.dropbox.com/s/83tbgvlxjp652hn/12rokte%20vese%20zay.mp3?dl=0"
+  },
+   {
+    name: "এই নীলিমায় বল",
+    artist: "মশিউর রহমান",
+    album: "কামিনী ফুটেছে বনে",
+    image: "../images/mosque.jpg",
+    path: "https://www.dropbox.com/s/cxrq5dequbvny6b/13oi%20nilimay%20bol.mp3?dl=0"
+  },
+  {
+    name: "বৃষ্টির ফোটায়",
+    artist: "মশিউর রহমান",
+    album: "কামিনী ফুটেছে বনে",
+    image: "../images/nature.jpg",
+    path: "https://dl.dropbox.com/s/nzt3mm2hc5xcxhd/14kon%20dale%20bujhi.mp3?dl=0"
+  },
+   {
+    name: "কোন ডালে বুঝি",
+    artist: "মশিউর রহমান",
+    album: "কামিনী ফুটেছে বনে",
+    image: "../images/sajek.jpg",
+    path: "https://dl.dropbox.com/s/ryipdkiic1kx7yu/15brristir%20fotay%20fotay.mp3?dl=0"
+  },
+];
