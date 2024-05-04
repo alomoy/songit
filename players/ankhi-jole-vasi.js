361b38e99f5b9f8497671dbd1da06c1f07b5f627
alomoy <alomoyinfo@gmail.com>
2024-05-04 10:38:25 +0600

    Add ankhi jole vasi

---
+++ players/ankhi-jole-vasi.js
@@ -0,0 +1,87 @@
+// Define the tracks that have to be played
+let track_list = [
+  {
+    name: "ছুম্মা লাতুছআলুন্না",
+    artist: "প্রত্যয় শিল্পি গোষ্ঠী",
+    album: "আঁখি জলে ভাসি",
+    image: "../images/mount.jpg",
+    path: "https://dl.dropbox.com/s/klwaoff217fec78/1summa.mp3"
+  },
+   {
+    name: "আঁখি জলে ভাসি",
+    artist: "ARTIST",
+    album: "আঁখি জলে ভাসি",
+    image: "../images/rain.jpg",
+    path: "https://dl.dropbox.com/s/yzz49zxq3y9xx5e/2ankhi%20jole.mp3"
+  },
+  {
+    name: "এমন কিছু গান আছে",
+    artist: "ARTIST",
+    album: "আঁখি জলে ভাসি",
+    image: "../images/song.png",
+    path: "https://dl.dropbox.com/s/e5rw5vclkqnm5m3/3emon%20kichu.mp3"
+  },
+  {
+    name: "মেঘেরও আড়ালে চাঁদের",
+    artist: "ARTIST",
+    album: "আঁখি জলে ভাসি",
+    image: "../images/moon.jpg",
+    path: "https://dl.dropbox.com/s/0dreeem3npzbd0u/4meghero%20arale.mp3"
+  },
+  {
+    name: "মা জননী প্রিয় সে মুখ",
+    artist: "ARTIST",
+    album: "আঁখি জলে ভাসি",
+    image: "../images/mom.jpg",
+    path: "https://dl.dropbox.com/s/2cwdedpcyxr31yc/5ma%20jononi.mp3"
+  },
+  {
+    name: "ও ঝর্ণা ধারা বল না",
+    artist: "ARTIST",
+    album: "আঁখি জলে ভাসি",
+    image: "../images/falls.jpg",
+    path: "https://dl.dropbox.com/s/p2ztksu6h47mlv4/6jhorna%20dhara.mp3"
+  },
+   {
+    name: "আল্লাহুম্মাগফিরলি",
+    artist: "ARTIST",
+    album: "আঁখি জলে ভাসি",
+    image: "../images/allahu.jpg",
+    path: "https://dl.dropbox.com/s/linp8mnu7ymb263/7allahummagfirli.mp3"
+  },
+  {
+    name: "স্বপ্নে ভরা স্মৃতিতে",
+    artist: "ARTIST",
+    album: "আঁখি জলে ভাসি",
+    image: "../images/light.jpg",
+    path: "https://dl.dropbox.com/s/e3ua5pa7848f2x5/8swopne%20vora.mp3"
+  },
+   {
+    name: "কেউ কেউ ভুলে",
+    artist: "ARTIST",
+    album: "আঁখি জলে ভাসি",
+    image: "../images/mount.jpg",
+    path: "https://dl.dropbox.com/s/skn7hrbgrv7y8lb/9keu%20keu.mp3"
+  },
+  {
+    name: "ধৈর্য্য শক্তি সাহস",
+    artist: "ARTIST",
+    album: "আঁখি জলে ভাসি",
+    image: "../images/tunnel.jpg",
+    path: "https://dl.dropbox.com/s/wwfectek5t0dcfz/10dhoirzo%20shokti.mp3"
+  },
+   {
+    name: "দ্বীনের পথে মোরা",
+    artist: "ARTIST",
+    album: "আঁখি জলে ভাসি",
+    image: "../images/mosque.jpg",
+    path: "https://dl.dropbox.com/s/9x4b0p668uj4a3i/11din%20bodole.mp3"
+  },
+  {
+    name: "সমৃদ্ধ বাংলাদেশ",
+    artist: "ARTIST",
+    album: "আঁখি জলে ভাসি",
+    image: "../images/kite.jpg",
+    path: "https://dl.dropbox.com/s/5y5req8b8sipam3/12somriddho%20bd.mp3"
+  },
+];
