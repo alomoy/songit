2d24dd1a96e844ea20c033f731d41703734f43c8
alomoy <alomoyinfo@gmail.com>
2024-05-04 12:02:12 +0600

    Add shilpir tuli

---
+++ players/shilpir-tuli.js
@@ -0,0 +1,101 @@
+// Define the tracks that have to be played
+let track_list = [
+  {
+    name: "দ্বীনের পথে দেখো",
+    artist: "প্রবাহ সাহিত্য সাংস্কৃতিক সংসদ",
+    album: "শিল্পীর তুলি",
+    image: "../images/mosque.jpg",
+    path: "http://probaho.weebly.com/uploads/1/0/9/5/10954712/diner_pothe_dekho_oi_deke_jai.mp3"
+  },
+   {
+    name: "তোমার নামে আমি",
+    artist: "প্রবাহ সাহিত্য সাংস্কৃতিক সংসদ",
+    album: "শিল্পীর তুলি",
+    image: "../images/allahu.jpg",
+    path: "http://probaho.weebly.com/uploads/1/0/9/5/10954712/tomar_name_ami_tashbi_pori.mp3"
+  },
+  {
+    name: "সীসাঢালা প্রাচীর",
+    artist: "প্রবাহ সাহিত্য সাংস্কৃতিক সংসদ",
+    album: "শিল্পীর তুলি",
+    image: "../images/fist.jpg",
+    path: "http://probaho.weebly.com/uploads/1/0/9/5/10954712/shesha_dhala_prachir_jeno.mp3"
+  },
+  {
+    name: "স্বাধীনতা",
+    artist: "প্রবাহ সাহিত্য সাংস্কৃতিক সংসদ",
+    album: "শিল্পীর তুলি",
+    image: "../images/kite.jpg",
+    path: "http://probaho.weebly.com/uploads/1/0/9/5/10954712/sadinota_lokhkho_shohider.mp3"
+  },
+  {
+    name: "কত কারুকাজ ছড়িয়ে",
+    artist: "প্রবাহ সাহিত্য সাংস্কৃতিক সংসদ",
+    album: "শিল্পীর তুলি",
+    image: "../images/kite.jpg",
+    path: "http://probaho.weebly.com/uploads/1/0/9/5/10954712/koto_karukaj_choria_dile_moha_bisshe.mp3"
+  },
+  {
+    name: "আল্লাহু আল্লাহু",
+    artist: "প্রবাহ সাহিত্য সাংস্কৃতিক সংসদ",
+    album: "শিল্পীর তুলি",
+    image: "../images/allahu.jpg",
+    path: "http://probaho.weebly.com/uploads/1/0/9/5/10954712/allaho_allaho_allaho.mp3"
+  },
+   {
+    name: "বয়ে চলা নদী",
+    artist: "প্রবাহ সাহিত্য সাংস্কৃতিক সংসদ",
+    album: "শিল্পীর তুলি",
+    image: "../images/river.jpg",
+    path: "http://probaho.weebly.com/uploads/1/0/9/5/10954712/boe_chola_nodi.mp3"
+  },
+  {
+    name: "আল্লাহতে যার পূর্ণ",
+    artist: "প্রবাহ সাহিত্য সাংস্কৃতিক সংসদ",
+    album: "শিল্পীর তুলি",
+    image: "../images/mosque.jpg",
+    path: "http://probaho.weebly.com/uploads/1/0/9/5/10954712/allahte_zar_porno_iman.mp3"
+  },
+   {
+    name: "হৃদয় আকাশ ভরা",
+    artist: "প্রবাহ সাহিত্য সাংস্কৃতিক সংসদ",
+    album: "শিল্পীর তুলি",
+    image: "../images/kite.jpg",
+    path: "http://probaho.weebly.com/uploads/1/0/9/5/10954712/ridoy_akas_vhora_sopner_rong.mp3"
+  },
+  {
+    name: "তুমি এলে তাই",
+    artist: "প্রবাহ সাহিত্য সাংস্কৃতিক সংসদ",
+    album: "শিল্পীর তুলি",
+    image: "../images/prophet.png",
+    path: "http://probaho.weebly.com/uploads/1/0/9/5/10954712/tomi_ale_tai_uthlo_heshe.mp3"
+  },
+   {
+    name: "সব মানুষের বন্ধু",
+    artist: "প্রবাহ সাহিত্য সাংস্কৃতিক সংসদ",
+    album: "শিল্পীর তুলি",
+    image: "../images/prophet.png",
+    path: "http://probaho.weebly.com/uploads/1/0/9/5/10954712/sob_manuser_bondu_tini.mp3"
+  },
+  {
+    name: "সবুজের ঢেউ আঁকে",
+    artist: "প্রবাহ সাহিত্য সাংস্কৃতিক সংসদ",
+    album: "শিল্পীর তুলি",
+    image: "../images/mount.jpg",
+    path: "http://probaho.weebly.com/uploads/1/0/9/5/10954712/sobujer_daqw_ake_ai_buke.mp3"
+  },
+   {
+    name: "শিল্পীর তুলিতে আঁকা",
+    artist: "প্রবাহ সাহিত্য সাংস্কৃতিক সংসদ",
+    album: "শিল্পীর তুলি",
+    image: "../images/mosque.jpg",
+    path: "http://probaho.weebly.com/uploads/1/0/9/5/10954712/shipir_tolite_aka.mp3"
+  },
+  {
+    name: "রঙধনু থেকে রং",
+    artist: "প্রবাহ সাহিত্য সাংস্কৃতিক সংসদ",
+    album: "শিল্পীর তুলি",
+    image: "../images/rainbow.jpg",
+    path: "http://probaho.weebly.com/uploads/1/0/9/5/10954712/rong_dhonu_theke_oi.mp3"
+  },
+];
