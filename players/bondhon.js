b99c3184b15343b987afb9ca629938d0654f9e02
alomoy <alomoyinfo@gmail.com>
2024-05-18 13:52:21 +0600

    Add bondhon

---
+++ players/bondhon.js
@@ -0,0 +1,108 @@
+// Define the tracks that have to be played
+let track_list = [
+  {
+    name: "বুকে বুকে তারা জ্বেলেছে",
+    artist: "সাইমুম ও পাঞ্জেরি শিল্পীগোষ্ঠী",
+    album: "বন্ধন",
+    image: "../images/fire.jpg",
+    path: "https://www.dropbox.com/s/4lbk0j3k9mm57hz/0-buke-buke.mp3?dl=1"
+  },
+   {
+    name: "তোমাদের ভুলিনি",
+    artist: "সাইমুম ও পাঞ্জেরি শিল্পীগোষ্ঠী",
+    album: "বন্ধন",
+    image: "../images/mujahid.jpg",
+    path: "https://www.dropbox.com/s/gpiph7773ajkf1w/1-tomader-vulini.mp3?dl=1"
+  },
+  {
+    name: "শান্তির পথে আজ",
+    artist: "সাইমুম ও পাঞ্জেরি শিল্পীগোষ্ঠী",
+    album: "বন্ধন",
+    image: "../images/nature.jpg",
+    path: "https://www.dropbox.com/s/gmkizsi4k70haxs/2-shantir-pothe-aj.mp3?dl=1"
+  },
+  {
+    name: "সত্যের এ পথে চলো",
+    artist: "সাইমুম ও পাঞ্জেরি শিল্পীগোষ্ঠী",
+    album: "বন্ধন",
+    image: "../images/mtroad.jpg",
+    path: "https://www.dropbox.com/s/ew8p3dnbtglf0fn/3-sotyer-e-pothe.mp3?dl=1"
+  },
+  {
+    name: "বাজিছে দামামা বাঁধরে",
+    artist: "সাইমুম ও পাঞ্জেরি শিল্পীগোষ্ঠী",
+    album: "বন্ধন",
+    image: "../images/horse.jpg",
+    path: "https://www.dropbox.com/s/zcv9zp31cz04nch/4-bajiche-damama.mp3?dl=1"
+  },
+   {
+    name: "দিয়েছি তপ্ত কলিজার ",
+    artist: "সাইমুম ও পাঞ্জেরি শিল্পীগোষ্ঠী",
+    album: "বন্ধন",
+    image: "../images/fire.jpg",
+    path: "https://www.dropbox.com/s/2zwvtruheydndzk/5-diye-zai-mora.mp3?dl=1"
+  },
+  {
+    name: "সালাম বীর জোয়ান",
+    artist: "সাইমুম ও পাঞ্জেরি শিল্পীগোষ্ঠী",
+    album: "বন্ধন",
+    image: "../images/mujahid.jpg",
+    path: "https://www.dropbox.com/s/9lfv1oi02hsxwan/6-salam-bir-jowan.mp3?dl=1"
+  },
+   {
+    name: "একটা ভাঙ্গা কলম দাও",
+    artist: "সাইমুম ও পাঞ্জেরি শিল্পীগোষ্ঠী",
+    album: "বন্ধন",
+    image: "../images/light.jpg",
+    path: "https://www.dropbox.com/s/wln1wea55kg0r6j/7-amay-ekti-vanga.mp3?dl=1"
+  },
+  {
+    name: "উদ্দাম মোরা দুর্বার",
+    artist: "সাইমুম ও পাঞ্জেরি শিল্পীগোষ্ঠী",
+    album: "বন্ধন",
+    image: "../images/horse.jpg",
+    path: "https://www.dropbox.com/s/a3w47xpwzz4rp1u/8-uddam-mora-durbar.mp3?dl=1"
+  },
+   {
+    name: "সকল ব্যাথার বোঝা",
+    artist: "সাইমুম ও পাঞ্জেরি শিল্পীগোষ্ঠী",
+    album: "বন্ধন",
+    image: "../images/dua.jpg",
+    path: "https://www.dropbox.com/s/wjizihfkmp2jivq/9-sokol-byathar-.mp3?dl=1"
+  },
+  {
+    name: "কিসের বিভেদ করো",
+    artist: "সাইমুম ও পাঞ্জেরি শিল্পীগোষ্ঠী",
+    album: "বন্ধন",
+    image: "../images/rainbow.jpg",
+    path: "https://www.dropbox.com/s/vh0br5tvensv5oa/11-kiser-bived-koro.mp3?dl=1"
+  },
+   {
+    name: "ও মাঝি ভাই...",
+    artist: "সাইমুম ও পাঞ্জেরি শিল্পীগোষ্ঠী",
+    album: "বন্ধন",
+    image: "../images/river2.jpg",
+    path: "https://www.dropbox.com/s/arqvsjuo2k6330p/10-o-majhi-vy.mp3?dl=1"
+  },
+  {
+    name: "সত্যের সংগ্রামে ফোটা",
+    artist: "সাইমুম ও পাঞ্জেরি শিল্পীগোষ্ঠী",
+    album: "বন্ধন",
+    image: "../images/mujahid.jpg",
+    path: "https://www.dropbox.com/s/rshny79e2yyrrvc/12-sotyer-songrame.mp3?dl=1"
+  },
+   {
+    name: "পথে নামার ডাক এসেছে",
+    artist: "সাইমুম ও পাঞ্জেরি শিল্পীগোষ্ঠী",
+    album: "বন্ধন",
+    image: "../images/mujahid.jpg",
+    path: "https://www.dropbox.com/s/ijtafouoroa2hsp/13-shono-dak-eseche.mp3?dl=1"
+  },
+  {
+    name: "আমি হারবো না",
+    artist: "সাইমুম ও পাঞ্জেরি শিল্পীগোষ্ঠী",
+    album: "বন্ধন",
+    image: "../images/fist.jpg",
+    path: "https://www.dropbox.com/s/qvqb1khf105yj1o/14-ami-harbona.mp3?dl=1"
+  },
+];
