916e1088931b56cd2b2575e8fdaaf5c5e1f052e6
alomoy <alomoyinfo@gmail.com>
2024-05-25 06:57:38 +0600

    Add alor prova

---
+++ players/alor-prova.js
@@ -0,0 +1,87 @@
+// Define the tracks that have to be played
+let track_list = [
+  {
+    name: "আমি তোমার প্রেমের",
+    artist: "সাইমুম শিল্পীগোষ্ঠী",
+    album: "আলোর প্রভা",
+    image: "../images/album/alor-prova.jpg",
+    path: "https://www.dropbox.com/scl/fi/wpvjvzowfhj8r105hbg0y/1-ami-tomar-premer.mp3?rlkey=jxn2cyb5ga9fposawim0jfgu8&st=kgfcbfxi&dl=1"
+  },
+   {
+    name: "হাজার দেশের এই",
+    artist: "সাইমুম শিল্পীগোষ্ঠী",
+    album: "আলোর প্রভা",
+    image: "../images/village_river.jpg",
+    path: "https://www.dropbox.com/scl/fi/nlur1ebydnyjs8ilb049x/2-hajar-desher-ei.mp3?rlkey=nozbqa3hzxdb196bvbst5d470&st=ihyogprv&dl=1"
+  },
+  {
+    name: "নামে তোমার মধু",
+    artist: "সাইমুম শিল্পীগোষ্ঠী",
+    album: "আলোর প্রভা",
+    image: "../images/prophet.png",
+    path: "https://www.dropbox.com/scl/fi/ndr4wu5wv7759akz14wqg/3-name-tomar-modhu.mp3?rlkey=g4n1gcfkcrcwuyfcsv1j6vhr3&st=z64jzs33&dl=1"
+  },
+  {
+    name: "কত ঠিকানাহীন",
+    artist: "সাইমুম শিল্পীগোষ্ঠী",
+    album: "আলোর প্রভা",
+    image: "../images/nature.jpg",
+    path: "https://www.dropbox.com/scl/fi/7t2tjl5pl5ny5mmfz5ck2/4-koto-thikanahin.mp3?rlkey=v1x2493mq5ljasc0oue2tku8a&st=5cobrld4&dl=1"
+  },
+  {
+    name: "প্রিয় আমার দেশের",
+    artist: "সাইমুম শিল্পীগোষ্ঠী",
+    album: "আলোর প্রভা",
+    image: "../images/beel.jpg",
+    path: "https://www.dropbox.com/scl/fi/net3jsklww7evg1kdfguv/5-priyo-amar-desher.mp3?rlkey=zkox8tt1nbuqw7x81gcoksjz2&st=pn2ytoui&dl=1"
+  },
+  {
+    name: "পরের জন্য করলে",
+    artist: "সাইমুম শিল্পীগোষ্ঠী",
+    album: "আলোর প্রভা",
+    image: "../images/mount.jpg",
+    path: "https://www.dropbox.com/scl/fi/3w559uqyolqgy43c667zr/6-porer-jonyo-korle.mp3?rlkey=7xw86tlpxbfsuxezokw22pnwh&st=hviet5k5&dl=1"
+  },
+   {
+    name: "মা তোমাকে নিয়ে",
+    artist: "সাইমুম শিল্পীগোষ্ঠী",
+    album: "আলোর প্রভা",
+    image: "../images/mom.jpg",
+    path: "https://www.dropbox.com/scl/fi/h6ugalgaapy3zpoovreeh/7-ma-tomake-niye.mp3?rlkey=enf60fv8b736titij2ec8ew11&st=js4alebg&dl=1"
+  },
+  {
+    name: "শিক্ষার আলো",
+    artist: "সাইমুম শিল্পীগোষ্ঠী",
+    album: "আলোর প্রভা",
+    image: "../images/light.jpg",
+    path: "https://www.dropbox.com/scl/fi/m8j6l9daf4gk9drfqvk2s/8-shikkhar-alo.mp3?rlkey=bvs3kr1w2v2r8uqkpfg5b0liq&st=nhu3ghty&dl=1"
+  },
+   {
+    name: "আমি যদি কোনোদিন",
+    artist: "সাইমুম শিল্পীগোষ্ঠী",
+    album: "আলোর প্রভা",
+    image: "../images/dua.jpg",
+    path: "https://www.dropbox.com/scl/fi/82hx6bxb20rmqhgotlw8t/9-ami-zodi-konodin.mp3?rlkey=bajc5gkvt578yxacm8z1giv1o&st=1cdwviqe&dl=1"
+  },
+  {
+    name: "ফেরেশতারা নিমেষহারা",
+    artist: "সাইমুম শিল্পীগোষ্ঠী",
+    album: "আলোর প্রভা",
+    image: "../images/light.jpg",
+    path: "https://www.dropbox.com/scl/fi/t5zhn7t56ty5bnz2xh275/10-ferestara-nimesh.mp3?rlkey=iqeed87pd8u6ggkl1g6n7fcl0&st=o7gr4w6m&dl=1"
+  },
+   {
+    name: "হাজার নদী মাঠ",
+    artist: "সাইমুম শিল্পীগোষ্ঠী",
+    album: "আলোর প্রভা",
+    image: "../images/nature.jpg",
+    path: "https://www.dropbox.com/scl/fi/jglb48627hqbgf1i9icoe/11-hajar-nodi-math.mp3?rlkey=rblxigk6qvvpi9u0k53s5s6ju&st=2tcer9lc&dl=1"
+  },
+  {
+    name: "আল্লাহ মহান",
+    artist: "সাইমুম শিল্পীগোষ্ঠী",
+    album: "আলোর প্রভা",
+    image: "../images/allahu.jpg",
+    path: "https://www.dropbox.com/scl/fi/8cxw8wk6v8hb9jrnqurro/12-Allah-mohan.mp3?rlkey=91wxiq3uos2msu1ak4r84mxn7&st=bf3wtqu8&dl=1"
+  },
+];
