c17316479af38477ffcb9e91c17f99c19fdf0900
alomoy <alomoyinfo@gmail.com>
2024-06-10 10:01:20 +0600

    Add sonar nolok

---
+++ players/sonar-nolok.js
@@ -0,0 +1,94 @@
+// Define the tracks that have to be played
+let track_list = [
+  {
+    name: "আল্লাহ নামের",
+    artist: "বিকল্প সাহিত্য সাংস্কৃতিক সংসদ",
+    album: "সোনার নোলক",
+    image: "../images/wind.jpg",
+    path: "https://www.dropbox.com/s/ye9fc52cjtca9ix/01allah%20namer.mp3?dl=1"
+  },
+   {
+    name: "সোনার নোলক",
+    artist: "বিকল্প সাহিত্য সাংস্কৃতিক সংসদ",
+    album: "সোনার নোলক",
+    image: "../images/river.jpg",
+    path: "https://www.dropbox.com/s/kydctbjjwgmoc88/02amar%20mayer%20sonar%20nolok.mp3?dl=1"
+  },
+  {
+    name: "তুমি এনে দিলে",
+    artist: "বিকল্প সাহিত্য সাংস্কৃতিক সংসদ",
+    album: "সোনার নোলক",
+    image: "../images/mount.jpg",
+    path: "https://www.dropbox.com/s/g8plxp7b5j7a1fp/03tumi%20ene%20dile.mp3?dl=1"
+  },
+  {
+    name: "পাপী আমি তাপী",
+    artist: "বিকল্প সাহিত্য সাংস্কৃতিক সংসদ",
+    album: "সোনার নোলক",
+    image: "../images/dua.jpg",
+    path: "https://www.dropbox.com/s/8zxayirx0xoydqe/04papi%20ami.mp3?dl=1"
+  },
+  {
+    name: "গুছিয়ে রাখো আতর",
+    artist: "বিকল্প সাহিত্য সাংস্কৃতিক সংসদ",
+    album: "সোনার নোলক",
+    image: "../images/mosque.jpg",
+    path: "https://www.dropbox.com/s/rewu5thi7poc2bu/05guchiye%20rakho.mp3?dl=1"
+  },
+  {
+    name: "প্রভু তুমি দয়া করো",
+    artist: "বিকল্প সাহিত্য সাংস্কৃতিক সংসদ",
+    album: "সোনার নোলক",
+    image: "../images/dua.jpg",
+    path: "https://www.dropbox.com/s/c1epoif7bibb51p/06-provu%20tumi.mp3?dl=1"
+  },
+   {
+    name: "আমি ধন্য হলাম",
+    artist: "বিকল্প সাহিত্য সাংস্কৃতিক সংসদ",
+    album: "সোনার নোলক",
+    image: "../images/tunnel.jpg",
+    path: "https://www.dropbox.com/s/bbfal1unv2l8521/07ami%20dhonno.mp3?dl=1"
+  },
+  {
+    name: "এই ধরণী ঐ যে গগণ",
+    artist: "বিকল্প সাহিত্য সাংস্কৃতিক সংসদ",
+    album: "সোনার নোলক",
+    image: "../images/nature.jpg",
+    path: "https://www.dropbox.com/s/r7ma5z79see5b17/08ei%20dhoroni%20oi.mp3?dl=1"
+  },
+   {
+    name: "ভাটি গাঙের নাইয়া",
+    artist: "বিকল্প সাহিত্য সাংস্কৃতিক সংসদ",
+    album: "সোনার নোলক",
+    image: "../images/river2.jpg",
+    path: "https://www.dropbox.com/s/l3b6jmlvam7q53p/09vati%20gnger%20naiya.mp3?dl=1"
+  },
+  {
+    name: "চলে গেলে যে দিন",
+    artist: "বিকল্প সাহিত্য সাংস্কৃতিক সংসদ",
+    album: "সোনার নোলক",
+    image: "../images/prophet.jpg",
+    path: "https://www.dropbox.com/s/g6pkuc0dq6uo4dx/10cole%20gele%20zedin.mp3?dl=1"
+  },
+   {
+    name: "চলো না যাই",
+    artist: "বিকল্প সাহিত্য সাংস্কৃতিক সংসদ",
+    album: "সোনার নোলক",
+    image: "../images/village_river.jpg",
+    path: "https://www.dropbox.com/s/3yw63ljnwi89gf1/11-cholona%20zaiye.mp3?dl=1"
+  },
+  {
+    name: "মনেরই আয়নায়",
+    artist: "বিকল্প সাহিত্য সাংস্কৃতিক সংসদ",
+    album: "সোনার নোলক",
+    image: "../images/falls.jpg",
+    path: "https://www.dropbox.com/s/7l4zx82w32xd6uo/12moneroi%20aynay.mp3?dl=1"
+  },
+   {
+    name: "বাংলাদেশের মাটিতে",
+    artist: "বিকল্প সাহিত্য সাংস্কৃতিক সংসদ",
+    album: "সোনার নোলক",
+    image: "../images/beel.jpg",
+    path: "https://www.dropbox.com/s/8med55g4onz03u6/13bder%20matite%20jonm.mp3?dl=1"
+  },
+];
