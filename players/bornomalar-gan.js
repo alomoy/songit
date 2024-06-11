473fea229c8f178c6e588bb4969c9349c06f19e4
alomoy <alomoyinfo@gmail.com>
2024-06-11 12:49:48 +0600

    Add bornomalar gan

---
+++ players/bornomalar-gan.js
@@ -0,0 +1,94 @@
+// Define the tracks that have to be played
+let track_list = [
+  {
+    name: "সালাম লিখেছে মা",
+    artist: "অনুপম শিল্পীগোষ্ঠী",
+    album: "বর্ণমালার গান",
+    image: "../images/bangla.jpg",
+    path: "https://www.dropbox.com/s/0udy0ojc2du0l4j/1salam%20likheche.mp3?dl=1"
+  },
+   {
+    name: "সোনালী হরফে",
+    artist: "অনুপম শিল্পীগোষ্ঠী",
+    album: "বর্ণমালার গান",
+    image: "../images/bangla.jpg",
+    path: "https://www.dropbox.com/s/hhzonbdasn6sf7b/2sonali%20horofe.mp3?dl=1"
+  },
+  {
+    name: "একুশ তুমি এসেছো",
+    artist: "অনুপম শিল্পীগোষ্ঠী",
+    album: "বর্ণমালার গান",
+    image: "../images/ekush.jpg",
+    path: "https://www.dropbox.com/s/x481un1bs4v7t0a/4ekush.mp3?dl=1"
+  },
+  {
+    name: "অ-তে অজু",
+    artist: "অনুপম শিল্পীগোষ্ঠী",
+    album: "বর্ণমালার গান",
+    image: "../images/bangla.jpg",
+    path: "https://www.dropbox.com/s/3ntugzssprl3cpz/3O%20te%20ozu.mp3?dl=1"
+  },
+  {
+    name: "মায়ের কোলে",
+    artist: "অনুপম শিল্পীগোষ্ঠী",
+    album: "বর্ণমালার গান",
+    image: "../images/mom.jpg",
+    path: "https://www.dropbox.com/s/e7gtqjb3ugqggcy/5mayer%20kole.mp3?dl=1"
+  },
+  {
+    name: "রক্ত চোখের",
+    artist: "অনুপম শিল্পীগোষ্ঠী",
+    album: "বর্ণমালার গান",
+    image: "../images/fist.jpg",
+    path: "https://www.dropbox.com/s/is65stfh93ghb6s/8rokto%20cokher.mp3?dl=1"
+  },
+   {
+    name: "একটা আশা নিয়ে",
+    artist: "অনুপম শিল্পীগোষ্ঠী",
+    album: "বর্ণমালার গান",
+    image: "../images/rain.jpg",
+    path: "https://www.dropbox.com/s/yqhw6dgg4amziui/7ekta%20asha.mp3?dl=1"
+  },
+  {
+    name: "মায়ের মুখের ভাষা",
+    artist: "অনুপম শিল্পীগোষ্ঠী",
+    album: "বর্ণমালার গান",
+    image: "../images/bangla.jpg",
+    path: "https://www.dropbox.com/s/cmjk0cohjg9y6jo/9mayer%20mukher%20vasha.mp3?dl=1"
+  },
+   {
+    name: "কত ভাষা আছে",
+    artist: "অনুপম শিল্পীগোষ্ঠী",
+    album: "বর্ণমালার গান",
+    image: "../images/hello.jpg",
+    path: "https://www.dropbox.com/s/4k4jc2udtfyoz7w/10koto%20vasha.mp3?dl=1"
+  },
+  {
+    name: "যে ভাষায় কথা বলি",
+    artist: "অনুপম শিল্পীগোষ্ঠী",
+    album: "বর্ণমালার গান",
+    image: "../images/bangla.jpg",
+    path: "https://www.dropbox.com/s/9jj8bvbbkmenvoi/11bangla%20vasha.mp3?dl=1"
+  },
+   {
+    name: "বাংলা ভাষা মাতৃভাষা",
+    artist: "অনুপম শিল্পীগোষ্ঠী",
+    album: "বর্ণমালার গান",
+    image: "../images/bangla.jpg",
+    path: "https://www.dropbox.com/s/doh5aumdwj73iqn/12matrivasha.mp3?dl=1"
+  },
+  {
+    name: "এই বাংলার পদ্মা",
+    artist: "অনুপম শিল্পীগোষ্ঠী",
+    album: "বর্ণমালার গান",
+    image: "../images/river2.jpg",
+    path: "https://www.dropbox.com/s/tkuqelxdv8ll0er/13banglar%20podma%20nodi.mp3?dl=1"
+  },
+   {
+    name: "সারা দুনিয়ায় বাংলা",
+    artist: "অনুপম শিল্পীগোষ্ঠী",
+    album: "বর্ণমালার গান",
+    image: "../images/banglaw.jpg",
+    path: "https://www.dropbox.com/s/6k94erra9ftocsc/bangla%20vashar%20nam.mp3?dl=1"
+  },
+];
