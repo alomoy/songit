838a2714712a23fc3b33143aa4def57d303e14ef
alomoy <alomoyinfo@gmail.com>
2024-06-11 13:23:19 +0600

    Add apon ghor

---
+++ players/apon-ghor.js
@@ -0,0 +1,94 @@
+// Define the tracks that have to be played
+let track_list = [
+  {
+    name: "ও দুনিয়ার মালিক",
+    artist: "রোকনুজ্জামান",
+    album: "আপন ঘর",
+    image: "../images/album/apon-ghor.jpg",
+    path: "https://www.dropbox.com/s/rw3lc9dlr2do2xx/1-o-duniyar-malilk.mp3?dl=1"
+  },
+   {
+    name: "ও আল্লাহ আমি",
+    artist: "রোকনুজ্জামান",
+    album: "আপন ঘর",
+    image: "../images/dua.jpg",
+    path: "https://www.dropbox.com/s/z531zijlgjypm0e/2-o-allah-ami.mp3?dl=1"
+  },
+  {
+    name: "যদি আমার গান",
+    artist: "রোকনুজ্জামান",
+    album: "আপন ঘর",
+    image: "../images/trek.jpg",
+    path: "https://www.dropbox.com/s/qv1fmd2kai5idx9/3-zodi-amar-gan.mp3?dl=1"
+  },
+  {
+    name: "এই দুনিয়া ছাড়তে",
+    artist: "রোকনুজ্জামান",
+    album: "আপন ঘর",
+    image: "../images/karakoram.jpg",
+    path: "https://www.dropbox.com/s/2p392c6fpttxphz/4-ei-duniya-charte.mp3?dl=1"
+  },
+  {
+    name: "বৃষ্টির ফোটার মতো",
+    artist: "রোকনুজ্জামান",
+    album: "আপন ঘর",
+    image: "../images/rain.jpg",
+    path: "https://www.dropbox.com/s/pwdyuz6flofyect/5-brristir-fotar-moto.mp3?dl=1"
+  },
+  {
+    name: "আল্লাহ তোমায়",
+    artist: "রোকনুজ্জামান",
+    album: "আপন ঘর",
+    image: "../images/allahu.jpg",
+    path: "https://www.dropbox.com/s/aew33denfneng5e/6-allah-tomay.mp3?dl=1"
+  },
+   {
+    name: "ও আমার দেশ",
+    artist: "রোকনুজ্জামান",
+    album: "আপন ঘর",
+    image: "../images/village_river.jpg",
+    path: "https://www.dropbox.com/s/l10y171u15108om/7-o-amar-desh.mp3?dl=1"
+  },
+  {
+    name: "আমার গানের সুরে",
+    artist: "রোকনুজ্জামান",
+    album: "আপন ঘর",
+    image: "../images/song.png",
+    path: "https://www.dropbox.com/s/9todbd8iriborpo/8-amar-ganer-sur.mp3?dl=1"
+  },
+   {
+    name: "কত দিন কত রাত",
+    artist: "রোকনুজ্জামান",
+    album: "আপন ঘর",
+    image: "../images/sajek.jpg",
+    path: "https://www.dropbox.com/s/f65ge1tgeqwl1wk/9-koto-din-koto-rat.mp3?dl=1"
+  },
+  {
+    name: "আল্লায় তোমায়",
+    artist: "রোকনুজ্জামান",
+    album: "আপন ঘর",
+    image: "../images/allahu.jpg",
+    path: "https://www.dropbox.com/s/utkezuyv8cc5ksa/10-allah-toamar.mp3?dl=1"
+  },
+   {
+    name: "নীলিমার ঐ দূর",
+    artist: "রোকনুজ্জামান",
+    album: "আপন ঘর",
+    image: "../images/mount.jpg",
+    path: "https://www.dropbox.com/s/sjw6m3yh5kxhetc/11-neelimar-oi-dur.mp3?dl=1"
+  },
+  {
+    name: "মাগো তোমার স্মৃতি",
+    artist: "রোকনুজ্জামান",
+    album: "আপন ঘর",
+    image: "../images/mom.jpg",
+    path: "https://www.dropbox.com/s/x91m3o95njtd7jz/12-mago-tomar-smriti.mp3?dl=1"
+  },
+   {
+    name: "সাজাইয়া দিয়া",
+    artist: "রোকনুজ্জামান",
+    album: "আপন ঘর",
+    image: "../images/mosque.jpg",
+    path: "https://www.dropbox.com/s/kse5gtys10anost/13-sajaiya-diya.mp3?dl=1"
+  },
+];
