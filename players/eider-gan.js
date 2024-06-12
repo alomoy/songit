2b2501eb9b620e4ff00a8589712a162a4b207434
alomoy <alomoyinfo@gmail.com>
2024-06-12 10:48:27 +0600

    Add Eider gan

---
+++ players/eider-gan.js
@@ -0,0 +1,101 @@
+// Define the tracks that have to be played
+let track_list = [
+  {
+    name: "মুখে মুখে সুখের কথা",
+    artist: "সাইমুম শিল্পীগোষ্ঠী",
+    album: " ঈদের গান",
+    image: "../images/eid.jpg",
+    path: "https://www.dropbox.com/s/xkcowk2ngbofily/1mukhe%20mukhe.mp3?dl=1"
+  },
+   {
+    name: "দল বেঁধে সব",
+    artist: "সাইমুম শিল্পীগোষ্ঠী",
+    album: " ঈদের গান",
+    image: "../images/children.jpg",
+    path: "https://www.dropbox.com/s/7d2ogvxkuowoncl/2dol%20bedhe%20sob.mp3?dl=1"
+  },
+  {
+    name: "ঈদ এলোরে",
+    artist: "সাইমুম শিল্পীগোষ্ঠী",
+    album: " ঈদের গান",
+    image: "../images/mount.jpg",
+    path: "https://www.dropbox.com/s/5a87f4x0an7zj5n/3eid%20elore.mp3?dl=1"
+  },
+  {
+    name: "অনুরাগে রঙিন ",
+    artist: "সাইমুম শিল্পীগোষ্ঠী",
+    album: " ঈদের গান",
+    image: "../images/rainbow.jpg",
+    path: "https://www.dropbox.com/s/qrps9lhu5o7r2dr/4onurange%20rongin.mp3?dl=1"
+  },
+  {
+    name: "ঈদ এসেছে",
+    artist: "সাইমুম শিল্পীগোষ্ঠী",
+    album: " ঈদের গান",
+    image: "../images/tunnel.jpg",
+    path: "https://www.dropbox.com/s/lrb8bhkqabb73x3/5eid%20eseche.mp3?dl=1"
+  },
+  {
+    name: "কুরবানি দাও কুরবানি",
+    artist: "সাইমুম শিল্পীগোষ্ঠী",
+    album: " ঈদের গান",
+    image: "../images/eid.jpg",
+    path: "https://www.dropbox.com/s/i5czln5f9qhrkr0/6qurbani%20dao.mp3?dl=1"
+  },
+   {
+    name: "আয়রে মুমিন",
+    artist: "সাইমুম শিল্পীগোষ্ঠী",
+    album: " ঈদের গান",
+    image: "../images/trek.jpg",
+    path: "https://www.dropbox.com/s/nv9zhucze1fg2oh/7ayre%20mumin.mp3?dl=1"
+  },
+  {
+    name: "ও মন রমজানের ঐ",
+    artist: "সাইমুম শিল্পীগোষ্ঠী",
+    album: " ঈদের গান",
+    image: "../images/ramadan.jpg",
+    path: "https://www.dropbox.com/s/nph13hh6qt2ahyi/8o%20mon%20romojaner.mp3?dl=1"
+  },
+   {
+    name: "ঈদ মানে তো খুশি",
+    artist: "সাইমুম শিল্পীগোষ্ঠী",
+    album: " ঈদের গান",
+    image: "../images/eid.jpg",
+    path: "https://www.dropbox.com/s/4iba2knn4qbm1vh/9eid%20mane%20to.mp3?dl=1"
+  },
+  {
+    name: "ঈদ এলো",
+    artist: "সাইমুম শিল্পীগোষ্ঠী",
+    album: " ঈদের গান",
+    image: "../images/nature.jpg",
+    path: "https://www.dropbox.com/s/zbqjirddlz3pvxa/10eid%20elo.mp3?dl=1"
+  },
+   {
+    name: "ঈদের খুশি",
+    artist: "সাইমুম শিল্পীগোষ্ঠী",
+    album: " ঈদের গান",
+    image: "../images/mtroad.jpg",
+    path: "https://www.dropbox.com/s/prbsuj8pjd9m4ed/11eider%20khushi.mp3?dl=1"
+  },
+  {
+    name: "ঈদ মানে তো নয়",
+    artist: "সাইমুম শিল্পীগোষ্ঠী",
+    album: " ঈদের গান",
+    image: "../images/moon.jpg",
+    path: "https://www.dropbox.com/s/y3pr7cgawyb2sve/12eid%20mane.mp3?dl=1"
+  },
+   {
+    name: "ঈদ নিয়ে আসে",
+    artist: "সাইমুম শিল্পীগোষ্ঠী",
+    album: " ঈদের গান",
+    image: "../images/mosque.jpg",
+    path: "https://www.dropbox.com/s/v0lh29blnboeefq/13eid%20niye%20ase.mp3?dl=1"
+  },
+  {
+    name: "আজ আনন্দ প্রতি",
+    artist: "সাইমুম শিল্পীগোষ্ঠী",
+    album: " ঈদের গান",
+    image: "../images/eid.jpg",
+    path: "https://www.dropbox.com/s/rw7sn0k60gvnvok/14aj%20anondo.mp3?dl=1"
+  },
+];
