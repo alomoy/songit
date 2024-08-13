cc70e45a855c5f07624ce9df8de9c621c65fd6d5
alomoy <ahmadbinrafiq@gmail.com>
2024-08-13 10:55:37 +0600

    Add shahidi gaan

---
+++ players/shahider-gaan.js
@@ -0,0 +1,94 @@
+// Define the tracks that have to be played
+let track_list = [
+  {
+    name: "আল কুর'আনকে ভালোবেসে",
+    artist: "",
+    album: "শহিদী গান",
+    image: "../images/quran.jpg",
+    path: "https://www.dropbox.com/s/xu7t9felu704up8/Al%20Quran%20ke.mp3?dl=1"
+  },
+   {
+    name: "আল্লাহর পথে যারা দিয়েছে জীবন",
+    artist: "",
+    album: "শহিদী গান",
+    image: "../images/trek.jpg",
+    path: "https://www.dropbox.com/s/sr5ovgic6sda5fb/Allahar%20Pathe%20Jara.mp3?dl=1"
+  },
+  {
+    name: "আল্লাহর পথে যারা জীবন দিল",
+    artist: "",
+    album: "শহিদী গান",
+    image: "../images/trail.jpg",
+    path: "https://www.dropbox.com/s/ab8syka7rgyuhp1/Allahar%20Pothe.mp3?dl=1"
+  },
+  {
+    name: "আল্লাহকে যারা বেসেছে ভালো ",
+    artist: "",
+    album: "শহিদী গান",
+    image: "../images/karakoram.jpg",
+    path: "https://www.dropbox.com/s/gnvrk09q68ildff/Allahke%20Jara.mp3?dl=1"
+  },
+  {
+    name: "দাও খোদা দাও হেথায়",
+    artist: "",
+    album: "শহিদী গান",
+    image: "../images/mtroad.jpg",
+    path: "https://www.dropbox.com/s/qi1va6yxew7bqik/Dao%20Khoda%20Dao.mp3?dl=1"
+  },
+   {
+    name: "দিন এলো আবার রক্ত ঝরাবার",
+    artist: "",
+    album: "শহিদী গান",
+    image: "../images/mount.jpg",
+    path: "https://www.dropbox.com/s/ywp3hqmdh9x00lk/Din%20Alo%20Abar.mp3?dl=1"
+  },
+  {
+    name: "দুয়ার ভেঙ্গে যে ছেলেটি মিছিলে",
+    artist: "",
+    album: "শহিদী গান",
+    image: "../images/horse.jpg",
+    path: "https://www.dropbox.com/s/3bjy4qus04dy40s/Duar%20venge.mp3?dl=1"
+  },
+   {
+    name: "প্রতি দিন সূর্য ওঠে",
+    artist: "",
+    album: "শহিদী গান",
+    image: "../images/desert.jpg",
+    path: "https://www.dropbox.com/s/ukvtvlj0apcyqva/Protidin%20Surjo.mp3?dl=1"
+  },
+  {
+    name: "রক্তই মূলত শক্তি",
+    artist: "",
+    album: "শহিদী গান",
+    image: "../images/fire.jpg",
+    path: "https://www.dropbox.com/s/8qziu9862gn02dq/rokto-e%20Muloto.mp3?dl=1"
+  },
+   {
+    name: "সেই সংগ্রামী মানুষের",
+    artist: "",
+    album: "শহিদী গান",
+    image: "../images/dua.jpg",
+    path: "https://www.dropbox.com/s/09pg7fnrhst8p71/Sei%20Songrami.mp3?dl=1"
+  },
+  {
+    name: "শহীদে শহীদে জনপদ",
+    artist: "",
+    album: "শহিদী গান",
+    image: "../images/horse.jpg",
+    path: "https://www.dropbox.com/s/rgq4kp1ylz2qtea/Shahide%20Shahide.mp3?dl=1"
+  },
+   {
+    name: "শত শত মালেক ওই",
+    artist: "",
+    album: "শহিদী গান",
+    image: "../images/mosque.jpg",
+    path: "https://www.dropbox.com/s/6xd6xo7lhib654a/Shoto%20shoto%20malek.mp3?dl=1"
+  },
+  {
+    name: "তোমরা যারা আজ",
+    artist: "",
+    album: "শহিদী গান",
+    image: "../images/nature.jpg",
+    path: "https://www.dropbox.com/s/ah1catwrxi1g2ko/Tomra%20Jara%20Aj.mp3?dl=1"
+  },
+];
