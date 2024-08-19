e0195a07470ad9277165bd51838647add1378ae9
alomoy <ahmadbinrafiq@gmail.com>
2024-08-19 08:29:00 +0600

    add naate rasul

---
+++ players/naate-rasul.js
@@ -0,0 +1,87 @@
+// Define the tracks that have to be played
+let track_list = [
+  {
+    name: "এলো কে কাবার",
+    artist: "সাইমুম শিল্পীগোষ্ঠী",
+    album: "নাতে রাসুল (সা)",
+    image: "../images/kaaba.webp",
+    path: "https://www.dropbox.com/scl/fi/oqk27z2zbuxn2bmvg5o7l/Elo-Ke-Kabar-Dhare.mp3?rlkey=d9x3jf9lzdwk7lcjat7x48la3&st=jyisom7i&dl=1"
+  },
+   {
+    name: "জেনে বুঝে গাইলো",
+    artist: "সাইমুম শিল্পীগোষ্ঠী",
+    album: "নাতে রাসুল (সা)",
+    image: "../images/nawawi.webp",
+    path: "https://www.dropbox.com/scl/fi/5ks2a86x74rnvcrn77dtu/Jene-Bujhe.mp3?rlkey=brrh6op2kzk34exmcv12fve32&st=0gno6d6x&dl=1"
+  },
+  {
+    name: "ওগো ও কামলিওয়ালা",
+    artist: "সাইমুম শিল্পীগোষ্ঠী",
+    album: "নাতে রাসুল (সা)",
+    image: "../images/nawawi.webp",
+    path: "https://www.dropbox.com/scl/fi/c7a57rw6rdxesqubyne37/Ogo-O-Kamli-Wala.mp3?rlkey=89vtr1rc0axxtq8dyjd1ox5ol&st=rr7t2c3b&dl=1"
+  },
+  {
+    name: "আমি যদি আরব",
+    artist: "সাইমুম শিল্পীগোষ্ঠী",
+    album: "নাতে রাসুল (সা)",
+    image: "../images/desert.jpg",
+    path: "https://www.dropbox.com/scl/fi/mg9gk9ydocdrln8war072/Ami-Jadi-Arab.mp3?rlkey=gl6zxjea2y4k5hcmdeyyla881&st=8scbwjzt&dl=1"
+  },
+  {
+    name: "ঘুমিয়ে আছো",
+    artist: "সাইমুম শিল্পীগোষ্ঠী",
+    album: "নাতে রাসুল (সা)",
+    image: "../images/nawawi.webp",
+    path: "https://www.dropbox.com/scl/fi/4m1vgmj7esn24r51vvjmc/Ghumiye-Achho.mp3?rlkey=aqn3cqraic2yo1zvbdtyhr4mt&st=yrjcbhai&dl=1"
+  },
+  {
+    name: "সালাম তোমাকে",
+    artist: "সাইমুম শিল্পীগোষ্ঠী",
+    album: "নাতে রাসুল (সা)",
+    image: "../images/nawawi.webp",
+    path: "https://www.dropbox.com/scl/fi/93p3do55e7piwzntlgzk5/Salam-Tomake.mp3?rlkey=y4m5yfqs9vvvmbk2yj7yh50cv&st=k277z6et&dl=1"
+  },
+   {
+    name: "লক্ষ চাঁদের আলো",
+    artist: "সাইমুম শিল্পীগোষ্ঠী",
+    album: "নাতে রাসুল (সা)",
+    image: "../images/nawawi.webp",
+    path: "https://www.dropbox.com/scl/fi/9rtv4nvuxg661uimcrcxn/Lakhha-Chander-Alo.mp3?rlkey=9vfy317lgda2v479cbhuisxhg&st=lchovgxg&dl=1"
+  },
+  {
+    name: "তালা'আল বাদরু",
+    artist: "সাইমুম শিল্পীগোষ্ঠী",
+    album: "নাতে রাসুল (সা)",
+    image: "../images/moon.jpg",
+    path: "https://www.dropbox.com/scl/fi/j56vde62eil1cdxtj3vkt/Tala-Al-Badru-Alaina.mp3?rlkey=oiuv1myqca8hmzam5d8zjuu5a&st=ce4g0dki&dl=1"
+  },
+   {
+    name: "তিনি নন তো শুধু",
+    artist: "সাইমুম শিল্পীগোষ্ঠী",
+    album: "নাতে রাসুল (সা)",
+    image: "../images/desert.jpg",
+    path: "https://www.dropbox.com/scl/fi/jq6z693duiyg7d68juwy3/Tini-Nonto-Shudhu.mp3?rlkey=rqmmj5eetmlddep8757sz053m&st=nggjy8cf&dl=1"
+  },
+  {
+    name: "তুমি রহমাতে আলা",
+    artist: "সাইমুম শিল্পীগোষ্ঠী",
+    album: "নাতে রাসুল (সা)",
+    image: "../images/nawawi.webp",
+    path: "https://www.dropbox.com/scl/fi/0szdjcynfmpn2bf6urqsr/Tumi-Rahmate.mp3?rlkey=g7gp0mfngvn7mrvi2alj9b0br&st=ekafe92y&dl=1"
+  },
+     {
+    name: "ওগো রাসুল প্রিয়",
+    artist: "সাইমুম শিল্পীগোষ্ঠী",
+    album: "নাতে রাসুল (সা)",
+    image: "../images/nawawi.webp",
+    path: "https://www.dropbox.com/scl/fi/bmdxhjb1j6robaw8d2vvq/Ogo-Rasul-Prio.mp3?rlkey=pch8rly2i8vtuk3590bpmzn4h&st=1qmoj91o&dl=1"
+  },
+   {
+    name: "ইয়া নাবী সালামু",
+    artist: "সাইমুম শিল্পীগোষ্ঠী",
+    album: "নাতে রাসুল (সা)",
+    image: "../images/prophet.png",
+    path: "https://www.dropbox.com/scl/fi/5wz44j21f7hpfvkoxamkq/Yaa-Nabi-Salamu.mp3?rlkey=jwcly7svvria6cbrt464dxuuc&st=v72g1ie7&dl=1"
+  },
+];
